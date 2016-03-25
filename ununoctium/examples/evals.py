7c7c765add77c919245128fd86e88268228b35c2
Scott Cyphers <scyphers@nervanasys.com>
2016-03-25 15:49:18 -0700

    Minor updates to correspond with graph meeting.

--- ununoctium/examples/evals.py
+++ ununoctium/examples/evals.py
@@ -10,8 +10,9 @@ with graph.default_graph(gr) as g:
 
     g.x = input((10,))
     g.y = input((10,))
+    g.w = deriv(g.x+g.y, g.y)
 
-    g.z = deriv(exp(abs(-log(g.x * g.y))), g.x)
+    g.z = 2*deriv(exp(abs(-log(g.x * g.y))), g.x)
 
 graph.show_graph(gr)
 
@@ -19,14 +20,14 @@ x = np.arange(10)+1
 y = x*x
 
 gnp = evaluation.GenNumPy(gr)
-gnp.evaluate(('z',), x=x, y=y)
+gnp.evaluate(('z','w'), x=x, y=y)
 
 enp = evaluation.NumPyEnvironment(gr)
-resultnp = enp.evaluate(('z',), x=x, y=y)
+resultnp = enp.evaluate(('z','w'), x=x, y=y)
 print resultnp
 
 epc = evaluation.PyCUDAEnvironment(gr)
-resultpc = epc.evaluate(('z',), x=x, y=y)
+resultpc = epc.evaluate(('z','w'), x=x, y=y)
 with cudagpu.cuda_device_context():
     print resultpc
 
