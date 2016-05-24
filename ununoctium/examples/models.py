Fix sum reduction axes in product deriv Combine some AST classes to simplify cifar_mlp with softmax, cross-entropy, L2 param regularization trains now.

ununoctium/examples/models.py
@@ -31,8 +31,8 @@ def linear(params, x, x_axes, axes, batch_axes=(), init=None):
     return be.dot(params.weights, x) + params.bias
 
 
-def affine(x, activation, **kargs):
-    return activation(linear(x, **kargs))
+def affine(x, activation, batch_axes=None, **kargs):
+    return activation(linear(x, batch_axes=batch_axes, **kargs), batch_axes=batch_axes)
 
 
 @be.with_name_context
@@ -57,9 +57,13 @@ def L2(x):
 
 
 def cross_entropy(y, t):
-    a = - be.log(y) * t
-    b = - be.log(1 - y) * (1 - t)
-    return be.sum(a + b)
+    """
+
+    :param y:  Estimate
+    :param t: Actual 1-hot data
+    :return:
+    """
+    return -be.sum(be.log(y) * t)
 
 
 class MyTest(be.Model):
@@ -84,7 +88,6 @@ class MyTest(be.Model):
         g.value = mlp(g.x, activation=be.softmax, x_axes=g.x.axes, shape_spec=layers, axes=g.y.axes, batch_axes=(g.N,),
                       init=uni)
 
-        #g.error = L2(g.y - g.value)
         g.error = cross_entropy(g.value, g.y)
 
         # L2 regularizer of parameters
@@ -98,6 +101,7 @@ class MyTest(be.Model):
 
         g.loss = g.error + .01 * reg
 
+
     @be.with_graph_context
     @be.with_environment
     def dump(self):
@@ -106,7 +110,11 @@ class MyTest(be.Model):
         g.x.value = be.ArrayWithAxes(np.empty((3, 32, 32, 128)), (g.C, g.H, g.W, g.N))
         g.y.value = be.ArrayWithAxes(np.empty((1000, 128)), (g.Y, g.N))
 
-        gnp = evaluation.GenNumPy(results=(g.error, g.value))
+        learning_rate = be.input(axes=())
+        params = g.error.parameters()
+        derivs = [be.deriv(g.error, param) for param in params]
+
+        gnp = evaluation.GenNumPy(results=derivs)
         gnp.evaluate()
 
     @be.with_graph_context
@@ -126,15 +134,13 @@ class MyTest(be.Model):
         g.W.length = w
         g.Y.length = train.nclasses
 
-        error = g.error
-        loss = g.loss
         learning_rate = be.input(axes=())
-        params = error.parameters()
-        derivs = [be.deriv(error, param) for param in params]
+        params = g.error.parameters()
+        derivs = [be.deriv(g.loss, param) for param in params]
 
         updates = be.doall(all=[be.decrement(param, learning_rate * deriv) for param, deriv in zip(params, derivs)])
 
-        enp = evaluation.NumPyEvaluator(results=[self.graph.value, error, loss, updates])
+        enp = evaluation.NumPyEvaluator(results=[self.graph.value, g.error, updates]+derivs)
         enp.initialize()
         for epoch in range(10):
             for mb_idx, (xraw, yraw) in enumerate(train):
@@ -146,7 +152,7 @@ class MyTest(be.Model):
                     print mb_idx
 
                 vals = enp.evaluate()
-                print(vals[loss].array/128.0)
+                print(vals[g.error].array/128.0)
                 # break
             train.reset()
             #print(be.get_current_environment().get_resolved_node_axes(g.value).array/128.0)
@@ -154,5 +160,5 @@ class MyTest(be.Model):
 
 
 y = MyTest()
-# y.dump()
+#y.dump()
 y.train()

ununoctium/geon/backends/graph/evaluation.py
@@ -63,6 +63,15 @@ class NumPyEvaluator(Evaluator):
         super(NumPyEvaluator, self).__init__(**kargs)
         self.rng = np.random.RandomState()
 
+    def trace(self, x, label, out):
+        oa = out.array
+        xa = x.array
+        if oa.shape == ():
+            oa = oa.reshape((1,))
+            xa = xa.reshape((1,))
+        oa[:] = xa
+        return out
+
     def constant(self, value, axes, dtype):
         return ArrayWithAxes(value, axes=axes, dtype=dtype)
 
@@ -184,12 +193,35 @@ class NumPyEvaluator(Evaluator):
     def sin(self, x, out):
         return np.sin(x.array_as_axes(out.axes), out=out.array)
 
-    def softmax(self, x, out):
-        xa = x.array_as_axes(out.axes)
-        m = xa.max()
+    def softmax(self, x, batch_axes, out):
+        softmax_axes = axes_sub(x.axes, batch_axes)
+        if softmax_axes == ():
+            raise ValueError('Empty softmax')
+        sa_i = ast.find_axes_in_axes(softmax_axes, x.axes)
+        if sa_i == -1:
+            raise ValueError('Softmax axes not contiguous')
+        if sa_i != 0:
+            raise ValueError('Softmax axes not on left')
+        xa = x.array
+        sm_dims = [axis.length for axis in softmax_axes]
+        def prod(dims):
+            result = 1
+            for dim in dims:
+                result = result * dim
+            return result
+        sm_size = prod(sm_dims)
+        rem_dims = [axis.length for axis in x.axes[len(softmax_axes):]]
+
+        if len(softmax_axes) > 1:
+            new_shape = [sm_size]+rem_dims
+            xa = xa.reshape(new_shape)
+        m = xa.max(axis=0)
+        m = m.reshape([1]*len(sm_dims)+rem_dims)
         np.subtract(xa, m, out=out.array)
         np.exp(out.array, out=out.array)
-        s = out.array.sum()
+        out_temp = out.array.reshape([sm_size]+list(out.array.shape[len(softmax_axes):]))
+        s = out_temp.sum(axis=0)
+        s = s.reshape([1]*len(sm_dims)+list(out.array.shape[len(softmax_axes):]))
         np.divide(out.array, s, out=out.array)
         return out
 
@@ -206,7 +238,7 @@ class NumPyEvaluator(Evaluator):
         x_axes = x.axes
         np_out_axes = axes_sub(x_axes, reduction_axes)
         np_red_dims = tuple(x_axes.index(axis) for axis in reduction_axes)
-        if out.axes != np_out_axes:
+        if list(out.axes) != list(np_out_axes):
             temp = np.sum(x.array, axis=np_red_dims)
             out.array[...] = temp
         else:
@@ -370,7 +402,7 @@ class GenNumPy(Evaluator):
                 vals[op] = val
                 body.append('{var} = {val} # Live={live}'.format(var=varname(op), val=val, live=live))
             else:
-                val = '{val} # Live={live}'.format(val=op.evaluate(self, varname(op.output), *args), live=live)
+                val = '{var} = {val} # Live={live}'.format(val=op.evaluate(self, varname(op.output), *args), var=varname(op), live=live)
                 vals[op] = val
                 body.append(val)
         for line in body:
@@ -430,6 +462,9 @@ class GenNumPy(Evaluator):
 
     def sin(self, x, out):
         return 'np.sin({x}, out={out})'.format(x=x, out=out)
+
+    def softmax(self, x, batch_axes, out):
+        return 'softmax({x}, batch_axes={batch_axes}, out=out'.format(x=x, batch_axes=batch_axes, out=out)
 
     def sqrt(self, x, out):
         return 'np.sqrt({x}, out={out})'.format(x=x, out=out)