232bb7bba28148fcdcd83652df148e7365de19ba
Hao Li <haoli@cs.umd.edu>
2016-07-07 15:51:37 -0700

    add test case for variable assignment with numpy tensor

--- ununoctium/tests/test_variable.py
+++ ununoctium/tests/test_variable.py
@@ -64,9 +64,39 @@ def test_init_1D_variable_from_numpy_array():
 
         print("pass numpy variable initialization")
 
+def test_assign_1D_variable_with_numpy_tensor():
+    with be.bound_environment():
+        ax.Y.length = 10
+        hello = be.Variable(axes=[ax.Y])
+        npvar = be.NumPyTensor(np.arange(ax.Y.length, dtype=np.float32), axes=[ax.Y])
+
+        hello = npvar
+        enp = be.NumPyTransformer(results=[hello])
+        result = enp.evaluate()[hello]
+        print(result)
+
+        print("pass 1D numpy tensor assignment")
+
+def test_assign_2D_variable_with_numpy_tensor():
+    with be.bound_environment():
+        ax.M.length = 3
+        ax.N.length = 2
+
+        var = be.Variable(axes=[ax.M, ax.N])
+        npvar = be.NumPyTensor(np.array([[1,1],[2,2],[3,3]]), axes=[ax.M, ax.N])
+
+        var = npvar
+        enp = be.NumPyTransformer(results=[var])
+        result = enp.evaluate()[var]
+        print(result)
+
+        print("pass 2D numpy tensor assignment")
 
 test_init_variable()
 test_init_1D_variable()
 test_init_2D_variable()
 test_init_1D_variable_from_numpy_array()
+
+test_assign_1D_variable_with_numpy_tensor()
+test_assign_2D_variable_with_numpy_tensor()
 
