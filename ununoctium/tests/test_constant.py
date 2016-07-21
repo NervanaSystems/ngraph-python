d7d8e35308a5122986dd25ed5f391beed17b33ce
Hao Li <haoli@cs.umd.edu>
2016-07-21 15:59:07 -0700

    more test cases for constant, constant multiply is not supported

--- ununoctium/tests/test_constant.py
+++ ununoctium/tests/test_constant.py
@@ -7,7 +7,7 @@ import geon.backends.graph.funs as be
 import geon.backends.graph.axis as ax
 import numpy as np
 from neon.initializers import Constant
-
+from geon.backends.graph.environment import Environment
 
 def test_constant_init():
     with be.bound_environment():
@@ -37,17 +37,52 @@ def test_constant_add():
         print("pass constant add")
 
 
-def test_2D_constant_init():
+def test_1D_constant_init():
     with be.bound_environment():
         ax.Y.length = 2
         a = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
         b = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
         c = a + b
-        enp = be.NumPyTransformer(results=[a, b, c])
+        enp = be.NumPyTransformer(results=[c])
         result = enp.evaluate()
         print(result[c])
 
+def test_2D_constant_init():
+    with be.bound_environment():
+        ax.M.length = 2
+        ax.N.length = 2
+        a = be.NumPyTensor(np.array([[1, 2],[3, 4]], dtype=np.float32), axes=[ax.M, ax.N])
+        b = be.NumPyTensor(np.array([[1, 2],[3, 4]], dtype=np.float32), axes=[ax.M, ax.N])
+        c = a + b
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+        print(result[c])
 
-test_constant_init()
-test_constant_add()
-test_2D_constant_init()
+def test_2D_constant_dot():
+    with be.bound_environment():
+        ax.M.length = 1
+        ax.N.length = 3
+        a = be.NumPyTensor(np.array([[1, 2, 3]], dtype=np.float32), axes=[ax.M, ax.N])
+
+        ax.N.length = 3
+        ax.Y.length = 2
+        b = be.NumPyTensor(np.array([[1,2], [2,3], [3,4]], dtype=np.float32), axes=[ax.N, ax.Y])
+        c = be.dot(a, b)
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+        print(result[c])
+
+def test_constant_multiply():
+    with be.bound_environment():
+        a = be.Constant([2, 4])
+        b = be.Constant(2)
+        c = be.multiply(a, b)
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+        print(result[c])
+
+# test_constant_init()
+# test_constant_add()
+# test_1D_constant_init()
+# test_2D_constant_init()
+test_constant_multiply()
