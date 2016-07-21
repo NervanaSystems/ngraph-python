0eadff348372f0f469da04aa343a7c06f2d3c214
Varun Kumar Vijay <varun@nervanasys.com>
2016-07-21 16:11:22 -0700

    Test

---
+++ ununoctium/tests/test_numeric_axis.py
@@ -0,0 +1,19 @@
+from geon.backends.graph.graph_test_utils import *
+
+
+@in_bound_environment
+def test_dot_with_numerics():
+    ax1 = be.NumericAxis(2)
+    ax2 = be.NumericAxis(2)
+    axes = Axes(ax1, ax2)
+
+    x_np = np.array([[1, 2], [1, 2]], dtype='float32')
+    x = be.NumPyTensor(x_np, axes=axes)
+
+    d = be.dot(x, x, numpy_matching=True)
+    d_val, = execute([d])
+
+    assert np.array_equal(d_val, np.dot(x_np, x_np))
+
+if __name__ == '__main__':
+    test_dot_with_numerics()
