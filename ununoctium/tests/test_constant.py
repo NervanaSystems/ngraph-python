7389babc4beb0dfd4de2a89277a83163c9d33bdc
Hao Li <haoli@cs.umd.edu>
2016-07-25 14:44:16 -0700

    fix styles

--- ununoctium/tests/test_constant.py
+++ ununoctium/tests/test_constant.py
@@ -22,8 +22,6 @@ import geon.backends.graph.funs as be
 import geon.backends.graph.axis as ax
 import numpy as np
 
-from neon.initializers import Constant
-from geon.backends.graph.environment import Environment
 
 def test_constant_init():
     with be.bound_environment():
@@ -45,50 +43,11 @@ def test_constant_add():
         c = a + b
 
         enp = be.NumPyTransformer(results=[c])
-
         result = enp.evaluate()[c]
         print(result)
-
-        assert (result == 3)
-        print("pass constant add")
+        assert result == 3
 
 
-def test_1D_constant_init():
-    with be.bound_environment():
-        ax.Y.length = 2
-        a = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
-        b = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
-        c = a + b
-        enp = be.NumPyTransformer(results=[c])
-        result = enp.evaluate()
-        print(result[c])
-
-def test_2D_constant_init():
-    with be.bound_environment():
-        ax.M.length = 2
-        ax.N.length = 2
-        a = be.NumPyTensor(np.array([[1, 2],[3, 4]], dtype=np.float32), axes=[ax.M, ax.N])
-        b = be.NumPyTensor(np.array([[1, 2],[3, 4]], dtype=np.float32), axes=[ax.M, ax.N])
-        c = a + b
-        enp = be.NumPyTransformer(results=[c])
-        result = enp.evaluate()
-        print(result[c])
-
-def test_2D_constant_dot():
-    with be.bound_environment():
-        ax.M.length = 1
-        ax.N.length = 3
-        a = be.NumPyTensor(np.array([[1, 2, 3]], dtype=np.float32), axes=[ax.M, ax.N])
-
-        ax.N.length = 3
-        ax.Y.length = 2
-        b = be.NumPyTensor(np.array([[1,2], [2,3], [3,4]], dtype=np.float32), axes=[ax.N, ax.Y])
-        c = be.dot(a, b)
-        enp = be.NumPyTransformer(results=[c])
-        result = enp.evaluate()
-        print(result[c])
-
-# be.Constant does not accept array initialization
 def test_constant_multiply():
     with be.bound_environment():
         a = be.Constant(4)
@@ -98,9 +57,52 @@ def test_constant_multiply():
         result = enp.evaluate()[c]
         assert result == 8
 
+
+def test_numpytensor_add():
+    with be.bound_environment():
+        ax.Y.length = 2
+        a = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
+        b = be.NumPyTensor(np.array([3, 5], dtype=np.float32), axes=[ax.Y])
+        c = a + b
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+        assert np.array_equal(result[c], [6, 10])
+
+        np_a = np.array([[1, 2], [3, 4]], dtype=np.float32)
+        np_b = np.array([[1, 2], [3, 4]], dtype=np.float32)
+        np_c = np_a + np_b
+
+        ax.M.length = 2
+        ax.N.length = 2
+        a = be.NumPyTensor(np_a, axes=[ax.M, ax.N])
+        b = be.NumPyTensor(np_b, axes=[ax.M, ax.N])
+        c = a + b
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+        assert np.array_equal(result[c], np_c)
+
+
+def test_numpytensor_dot():
+    with be.bound_environment():
+        np_a = np.array([[1, 2, 3]], dtype=np.float32)
+        np_b = np.array([[1, 2], [2, 3], [3, 4]], dtype=np.float32)
+        np_c = np.dot(np_a, np_b)
+
+        ax.M.length = 1
+        ax.N.length = 3
+        a = be.NumPyTensor(np_a, axes=[ax.M, ax.N])
+        ax.N.length = 3
+        ax.Y.length = 2
+        b = be.NumPyTensor(np_b, axes=[ax.N, ax.Y])
+        c = be.dot(a, b)
+        enp = be.NumPyTransformer(results=[c])
+        result = enp.evaluate()
+
+        assert np.array_equal(result[c], np_c)
+
+
 test_constant_init()
 test_constant_add()
-test_1D_constant_init()
-test_2D_constant_init()
 test_constant_multiply()
-
+test_numpytensor_add()
+test_numpytensor_dot()
