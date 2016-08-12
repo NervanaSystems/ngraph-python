fe0dcbe23c4fe0a65ae3dcfebb5f1f2643aef6e2
Hao Li <hao-intern@nervanasys.com>
2016-08-12 10:51:06 -0700

    fix unit test style

--- ununoctium/tests/test_transformer.py
+++ ununoctium/tests/test_transformer.py
@@ -1,6 +1,7 @@
 import geon as be
 import numpy as np
 
+
 def test_evalutaion_twice():
     """
     test executing a computation graph twice on a one layer MLP
@@ -18,7 +19,6 @@ def test_evalutaion_twice():
                                                  be.NumericAxis(1)))
     hidden1 = be.dot(x, hidden1_weights) + hidden1_biases
 
-
     trans = be.NumPyTransformer()
     comp = trans.computation([hidden1])
     trans.finalize()
@@ -26,4 +26,3 @@ def test_evalutaion_twice():
     result_1 = comp.evaluate()[hidden1]
     result_2 = comp.evaluate()[hidden1]
     assert np.array_equal(result_1, result_2)
-
