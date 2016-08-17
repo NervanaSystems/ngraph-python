0e67230e634084dd316bb385b694dd55b0a1cf1e
Yixing Lao <yixing.lao@gmail.com>
2016-08-17 16:30:55 -0700

    put grad, update, loss in one computation

--- ununoctium/examples/logistic_regression.py
+++ ununoctium/examples/logistic_regression.py
@@ -1,3 +1,18 @@
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
 from __future__ import print_function
 import numpy as np
 import geon as be
@@ -32,7 +47,7 @@ def numpy_logistic_regression(xs, ys, max_iter, alpha):
         # backward
         grad = get_grad(thetas, xs, ys)
         # print
-        print(grad, loss)
+        print("grad: %s, loss %s" % (grad, loss))
         # update
         thetas -= grad * alpha
 
@@ -81,16 +96,13 @@ def geon_logistic_regression(xs_np, ys_np, max_iter, alpha):
 
         # transformer
         transformer = be.NumPyTransformer()
-        train_comp = transformer.computation([update])
-        eval_comp = transformer.computation([loss])
+        train_eval_comp = transformer.computation([grad, update, loss])
         transformer.finalize()
 
         # evaluate
         for i in range(max_iter):
-            # print
-            print(eval_comp.evaluate()[loss])
-            # forward, backward, update all together
-            train_comp.evaluate()
+            result_dict = train_eval_comp.evaluate()
+            print("grad: %s, loss %s" % (result_dict[grad], result_dict[loss]))
 
 
 if __name__ == '__main__':
