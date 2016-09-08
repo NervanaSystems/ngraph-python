3b3eef9751e02df5b20052d10d9e9e724ad48e65
Hanlin Tang <hanlin@nervanasys.com>
2016-09-08 13:48:33 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 # ----------------------------------------------------------------------------
 from setuptools import setup, find_packages
 setup(
-    name="Graphiti",
+    name="nervanagraph",
     version="0.1",
     packages=find_packages(exclude=["tests"]),
     author='Nervana Systems',
