6aef65f9df3cff97714d5b69d98322aeab6a6ac9
Yixing Lao <yixing.lao@gmail.com>
2016-09-08 17:22:47 -0700

    remove freeze_graph.sh

--- ununoctium/examples/tensorflow_import/3rd_party/mnist/freeze_graph.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python ../freeze_graph.py \
-  --input_graph=graph.pb.txt \
-  --input_checkpoint=model.ckpt \
-  --output_graph=graph_froze.pb \
-  --output_node_names=softmax_linear/add
