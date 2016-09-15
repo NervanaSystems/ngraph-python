broadcast flatten bug

ngraph/transformers/nptransform.py
@@ -27,10 +27,10 @@ from ngraph.op_graph import arrayaxes  # noqa
 from ngraph.util.pygen import PyGen, indenting
 from ngraph.util.generics import generic_method
 
-from ngraph.op_graph.op_graph import absolute, Add, argmax, argmin, cos, Divide, Dot, equal, exp, \
-    greater, greater_equal, less, less_equal, log, max, maximum, min, minimum, multiply, \
-    negative, not_equal, onehot, power, reciprocal, SetItem, sign, sin, sqrt, square, subtract, \
-    sum, tanh, tensor_size, Fill, TensorDescription, Unslice, Stack, Dimshuffle
+from ngraph.op_graph.op_graph import absolute, Add, Argmax, Argmin, cos, Divide, Dot, Equal, exp, \
+    Greater, GreaterEqual, Less, LessEqual, log, Max, Maximum, Min, Minimum, Multiply, \
+    negative, NotEqual, onehot, Power, reciprocal, SetItem, sign, sin, sqrt, square, Subtract, \
+    Sum, tanh, tensor_size, Fill, TensorDescription, Unslice, Stack, Dimshuffle
 from ngraph.op_graph.convolution import convolution
 
 from ngraph.transformers.base import Transformer, DeviceBufferStorage, DeviceBufferReference, \
@@ -243,11 +243,11 @@ class NumPyCodeGenerator(PyGen):
     def generate_op(self, op, out, x, y):
         self.append("np.add({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(argmax)
+    @generate_op.on_type(Argmax)
     def generate_op(self, op, out, x):
         self.append("np.ndarray.argmax({}, 0, out={})", x, out)
 
-    @generate_op.on_type(argmin)
+    @generate_op.on_type(Argmin)
     def generate_op(self, op, out, x):
         self.append("np.ndarray.argmin({}, 0, out={})", x, out)
 
@@ -343,9 +343,15 @@ class NumPyCodeGenerator(PyGen):
 
     @generate_op.on_type(Dot)
     def generate_op(self, op, out, x, y):
-        self.append("np.dot({}, {}, out={})", x, y, out)
+        self.append("""
+        x, y, out = {}, {}, {}
+        try:
+            np.dot(x, y, out=out)
+        except ValueError:
+            np.dot(np.copy(x), np.copy(y), out=out)
+        """, x, y, out)
 
-    @generate_op.on_type(equal)
+    @generate_op.on_type(Equal)
     def generate_op(self, op, out, x, y):
         self.append("np.equal({}, {}, out={})", x, y, out)
 
@@ -357,19 +363,19 @@ class NumPyCodeGenerator(PyGen):
     def generate_op(self, op, out, x):
         self.append("{}.fill({})", x, op.scalar)
 
-    @generate_op.on_type(greater)
+    @generate_op.on_type(Greater)
     def generate_op(self, op, out, x, y):
         self.append("np.greater({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(greater_equal)
+    @generate_op.on_type(GreaterEqual)
     def generate_op(self, op, out, x, y):
         self.append("np.greater_equal({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(less)
+    @generate_op.on_type(Less)
     def generate_op(self, op, out, x, y):
         self.append("np.less({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(less_equal)
+    @generate_op.on_type(LessEqual)
     def generate_op(self, op, out, x, y):
         self.append("np.less_equal({}, {}, out={})", x, y, out)
 
@@ -377,29 +383,23 @@ class NumPyCodeGenerator(PyGen):
     def generate_op(self, op, out, x):
         self.append("np.log({}, out={})", x, out)
 
-    @generate_op.on_type(max)
+    @generate_op.on_type(Max)
     def generate_op(self, op, out, x):
-        if op.mode is 'copy':
-            self.append("{}.__setitem__((), {})", out, x)
-        else:
-            self.append("np.max({}, {}, out={})", x, op.mode, out)
+        self.append("np.max({}, 0, out={})", x, out)
 
-    @generate_op.on_type(maximum)
+    @generate_op.on_type(Maximum)
     def generate_op(self, op, out, x, y):
         self.append("np.maximum({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(min)
+    @generate_op.on_type(Min)
     def generate_op(self, op, out, x):
-        if op.mode is 'copy':
-            self.append("{}.__setitem__((), {})", out, x)
-        else:
-            self.append("np.min({}, {}, out={})", x, op.mode, out)
+        self.append("np.min({}, 0, out={})", x, out)
 
-    @generate_op.on_type(minimum)
+    @generate_op.on_type(Minimum)
     def generate_op(self, op, out, x, y):
         self.append("np.minimum({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(multiply)
+    @generate_op.on_type(Multiply)
     def generate_op(self, op, out, x, y):
         self.append("np.multiply({}, {}, out={})", x, y, out)
 
@@ -407,7 +407,7 @@ class NumPyCodeGenerator(PyGen):
     def generate_op(self, op, out, x):
         self.append("np.negative({}, out={})", x, out)
 
-    @generate_op.on_type(not_equal)
+    @generate_op.on_type(NotEqual)
     def generate_op(self, op, out, x, y):
         self.append("np.not_equal({}, {}, out={})", x, y, out)
 
@@ -421,7 +421,7 @@ class NumPyCodeGenerator(PyGen):
             o[x[i], i] = 1
         """, x=x, o=o)
 
-    @generate_op.on_type(power)
+    @generate_op.on_type(Power)
     def generate_op(self, op, out, x, y):
         self.append("np.power({}, {}, out={}", x, y, out)
 
@@ -462,16 +462,13 @@ class NumPyCodeGenerator(PyGen):
     def generate_op(self, op, out, x):
         self.append("np.square({}, out={})", x, out)
 
-    @generate_op.on_type(subtract)
+    @generate_op.on_type(Subtract)
     def generate_op(self, op, out, x, y):
         self.append("np.subtract({}, {}, out={})", x, y, out)
 
-    @generate_op.on_type(sum)
+    @generate_op.on_type(Sum)
     def generate_op(self, op, out, x):
-        if op.mode is 'copy':
-            self.append("{}.__setitem__((), {})", out, x)
-        else:
-            self.append("np.sum({}, axis={}, out={})", x, op.mode, out)
+        self.append("np.sum({}, axis=0, out={})", x, out)
 
     @generate_op.on_type(tanh)
     def generate_op(self, op, out, x):

tests/test_numpy.py
@@ -43,9 +43,10 @@ def test_constant_multiply():
 
 def test_constant_tensor_multiply():
     ax.Y.length = 2
+    ax.N.length = 2
 
-    a = ng.Constant(np.array([[1.0, 1.0], [1.0, 1.0]], dtype='float32'), axes=[ax.Y, ax.Y])
-    b = ng.Constant(np.array([[1.0, 1.0], [1.0, 1.0]], dtype='float32'), axes=[ax.Y, ax.Y])
+    a = ng.Constant(np.array([[1.0, 1.0], [1.0, 1.0]], dtype='float32'), axes=[ax.Y, ax.N])
+    b = ng.Constant(np.array([[1.0, 1.0], [1.0, 1.0]], dtype='float32'), axes=[ax.Y, ax.N])
 
     c = ng.multiply(a, b)
 
@@ -310,8 +311,6 @@ def test_elementwise_ops_unmatched_args():
     broadcast_dims = (ax.W.length, ax.H.length, 1)
 
     for np_op, be_op in ELEMENTWISE_BINARY_OPS:
-        if np_op is not np.divide:
-            continue
         # Matched sizes
         p_u = ng.placeholder(axes=sample_axes)
         p_v = ng.placeholder(axes=batch_axes)
@@ -341,6 +340,7 @@ def test_elementwise_ops_unmatched_args():
         dvudvnum_fun = ex.numeric_derivative(vu_op, p_v, .001, p_u)
         dvudvt_fun = ex.derivative(vu_op, p_v, p_u)
 
+        # u op v
         result_be = uv_fun(u, v)
         np.testing.assert_allclose(uv_np, result_be, atol=1e-4, rtol=1e-4)
         duvdunum = duvdunum_fun(u, v)