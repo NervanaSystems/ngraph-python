d3ecdb052c0bce914311b075b38585a6d2fe5fcd
Scott Cyphers <scott.cyphers@intel.com>
2016-10-25 18:58:41 -0700

    End negative costs.
    safelog isn't quite log, so faster comp needs to clip.

--- ngraph/frontends/neon/tests/test_examples.py
+++ ngraph/frontends/neon/tests/test_examples.py
@@ -23,10 +23,10 @@ example_dir = os.path.join(base_dir, '../../../../examples/')
 
 db = {'mnist': {'filename': os.path.join(example_dir, 'mnist_mlp.py'),
                 'arguments': '-e10 -r0',
-                'cost': 0.0839},
+                'cost': 0.0861108228564},
       'cifar10': {'filename': os.path.join(example_dir, 'cifar10_mlp.py'),
                   'arguments': '-e10 -r0',
-                  'cost': 2.25}
+                  'cost': 1.37536168098}
       }
 
 
@@ -71,10 +71,10 @@ def run_model(request, tmpdir_factory, transformer_factory):
     return (model_name, out1, out2)
 
 
-@pytest.mark.xfail(strict=True)
 def test_model_traincost(run_model, transformer_factory):
     (model_name, out_path, _) = run_model
     cost = get_last_epoch_cost(out_path)
+    expected_cost = db[model_name]['cost']
     np.testing.assert_allclose(cost, db[model_name]['cost'], rtol=0.15)
 
 
@@ -84,11 +84,8 @@ def test_model_positivecost(run_model, transformer_factory):
     """
     (model_name, out_path, _) = run_model
 
-    if model_name in ['mnist']:
-        pytest.xfail("negative costs still an error")
-    else:
-        cost = get_cost(out_path)
-        assert all(cost >= 0.0)
+    cost = get_cost(out_path)
+    assert all(cost >= 0.0)
 
 
 def test_model_simple(run_model, transformer_factory):
