532b30679dc5a6c703154c8cb61469fdf8e205b8
Jakub Grzybek <jakub.grzybek@intel.com>
2016-11-30 13:38:37 +0100

    remove unused itertools

--- tests/test_ordered.py
+++ tests/test_ordered.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------
-import itertools
 
 import pytest
 from ngraph.op_graph.op_graph import OrderedSet
