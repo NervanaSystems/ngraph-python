fix gradients of reduce_prod

ngraph/frontends/tensorflow/tests/test_ops_reduction.py
@@ -36,12 +36,12 @@ class Tester(ImporterTester):
 
         # test
         for reduction_indices in reduction_indices_list:
-            f = tf.reduce_sum(a, reduction_indices=reduction_indices)
-            self.run(f, tf_feed_dict=feed_dict)
-            f = tf.reduce_prod(a, reduction_indices=reduction_indices)
-            self.run(f, tf_feed_dict=feed_dict)
-            f = tf.reduce_mean(a, reduction_indices=reduction_indices)
-            self.run(f, tf_feed_dict=feed_dict)
+            f_sum = tf.reduce_sum(a, reduction_indices=reduction_indices)
+            self.run(f_sum, tf_feed_dict=feed_dict)
+            f_prod = tf.reduce_prod(a, reduction_indices=reduction_indices)
+            self.run(f_prod, tf_feed_dict=feed_dict)
+            f_mean = tf.reduce_mean(a, reduction_indices=reduction_indices)
+            self.run(f_mean, tf_feed_dict=feed_dict)
 
     def test_sum_prod_broadcast(self):
         # placeholder

ngraph/transformers/nptransform.py
@@ -36,7 +36,8 @@ from ngraph.op_graph.op_graph import AbsoluteOneDOp, AddOneDim, AddZeroDim, Argm
     LessEqualOneDim, LessEqualZeroDim, LogOneDOp, Max, MaximumOneDim, MaximumZeroDim, Min, \
     MinimumOneDim, MinimumZeroDim, \
     MultiplyOneDim, MultiplyZeroDim, \
-    NegativeOneDOp, NotEqualOneDim, NotEqualZeroDim, OneHotOp, Power, ReciprocalOneDOp, \
+    NegativeOneDOp, NotEqualOneDim, NotEqualZeroDim, OneHotOp, ReciprocalOneDOp, \
+    Power, PowerZeroDim, \
     AssignOneDOp, SignOneDOp, SinOneDOp, SqrtOneDOp, SquareOneDOp, RngOp, \
     SubtractOneDim, SubtractZeroDim, \
     Sum, Prod, TanhOneDOp, TensorSizeOp, Fill, TensorDescription, \
@@ -583,7 +584,11 @@ class NumPyCodeGenerator(PyGen):
 
     @generate_op.on_type(Power)
     def generate_op(self, op, out, x, y):
-        self.append("np.power({}, {}, out={}", x, y, out)
+        self.append("np.power({}, {}, out={})", x, y, out)
+
+    @generate_op.on_type(PowerZeroDim)
+    def generate_op(self, op, out, x, y):
+        self.append("np.power({}, {}, out={})", x, y, out)
 
     @generate_op.on_type(PrintOp)
     def generate_op(self, op, out, x):