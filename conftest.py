99a8cca1ce5a82bc98326c89abba234ea79d5f7c
Yixing Lao <yixing.lao@gmail.com>
2016-12-27 18:43:18 -0800

    add py.test --enable_flex in conftest

--- conftest.py
+++ conftest.py
@@ -16,12 +16,28 @@ import pytest
 import ngraph.transformers as ngt
 
 
+def pytest_addoption(parser):
+    parser.addoption("--enable_flex", action="store_true",
+                     help="Enable and *only* enable flexgpu transformer.")
+
 @pytest.fixture(scope="module",
                 params=ngt.transformer_choices())
 def transformer_factory(request):
-    factory = ngt.make_transformer_factory(request.param)
-    ngt.set_transformer_factory(factory)
-    yield factory
+    if pytest.config.getoption("--enable_flex"):
+        # Only yield flex if flex is available
+        if 'flexgpu' in ngt.transformer_choices():
+            factory = ngt.make_transformer_factory('flexgpu')
+            ngt.set_transformer_factory(factory)
+            yield factory
+        else:
+            raise ValueError("GPU not found, should not set --enable_flex"
+                             "flag for py.test.")
+    else:
+        # Skip flex if it's in request.param
+        if request.param != 'flexgpu':
+            factory = ngt.make_transformer_factory(request.param)
+            ngt.set_transformer_factory(factory)
+            yield factory
 
     # Reset transformer factory to default
     ngt.set_transformer_factory(ngt.make_transformer_factory("numpy"))
