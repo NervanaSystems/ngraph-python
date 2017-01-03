93b8a60975772efc85f45abbbf37f1b30fbfb85d
Jacek Reniecki <jacek.reniecki@intel.com>
2017-01-03 08:12:29 -0500

    caffe2 fe - verbose=False when parsing net

--- ngraph/frontends/caffe2/tests/test_ops_nn.py
+++ ngraph/frontends/caffe2/tests/test_ops_nn.py
@@ -122,7 +122,7 @@ def test_LabelCrossEntropy():
 
     # Import caffe2 network into ngraph
     importer = C2Importer()
-    importer.parse_net_def(net.Proto(), verbose=True)
+    importer.parse_net_def(net.Proto(), verbose=False)
 
     # Get handle
     f_ng = importer.get_op_handle("xent")
