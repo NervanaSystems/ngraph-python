d49c85bf54cbaaa919e4db8fd3ca8021720d2056
Alex Park <alex.park@gmail.com>
2017-02-02 16:36:31 -0800

    Clean up several neon frontend unit tests to reduce redundant code

--- ngraph/frontends/neon/tests/test_batchnorm_layer.py
+++ ngraph/frontends/neon/tests/test_batchnorm_layer.py
@@ -15,61 +15,48 @@
 '''
 Test of the batchnorm layer
 '''
-import itertools as itt
-
-import numpy as np
 import pytest
+import numpy as np
 import ngraph as ng
 from ngraph.frontends.neon import BatchNorm
 from ngraph.testing.execution import ExecutorFactory
 
 
-def pytest_generate_tests(metafunc):
-    bsz_rng = [32, 64]
-
-    if 'basic_bnargs' in metafunc.fixturenames:
-        nin_rng = [4, 10, 32]
-        fargs = itt.product(nin_rng, bsz_rng)
-        metafunc.parametrize('basic_bnargs', fargs)
-
-
-@pytest.mark.skip(reason="Need value_op to correctly check side-effects")
-def test_batchnorm_fprop(basic_bnargs, transformer_factory):
+@pytest.mark.parametrize("batch_size", [32, 64])
+@pytest.mark.parametrize("input_size", [4, 10, 32])
+@pytest.mark.parametrize("rho,epsilon", [(0.2, 0.1)])
+def test_batchnorm_fprop(batch_size, input_size, rho, epsilon, transformer_factory):
     # This checks that that we are doing batch norm across a feature make_axis
     # and properly tracking the side effect variables
-    nin, batch_size = basic_bnargs
-
+    np.random.seed(0)
     # set inputs
-    N = ng.make_axis(batch_size, batch=True).named('N')
-    F = ng.make_axis(nin).named('F')
+    N = ng.make_axis(batch_size, batch=True)
+    F = ng.make_axis(input_size)
 
-    rho, eps = 0.2, 0.1
-    inp = ng.placeholder([F, N])
-    layer = BatchNorm(rho, eps)
-    fprop = layer.train_outputs(inp)
+    input_placeholder = ng.placeholder([F, N])
+    layer = BatchNorm(rho, epsilon)
+    fprop = layer.train_outputs(input_placeholder)
 
     with ExecutorFactory() as ex:
-        fprop_function = ex.executor(fprop, inp)
-        np.random.seed(0)
+        fprop_function = ex.transformer.computation(fprop, input_placeholder)
+        stats_function = ex.transformer.computation([ng.value_of(layer.gmean),
+                                                     ng.value_of(layer.gvar)])
 
         # initial conditions for tracked variables
         gmean_ref, gvar_ref = 0.0, 1.0
 
         # create data
         for i in range(2):
-            x = np.random.random((nin, batch_size)).astype(np.float32)
+            x = np.random.random((input_size, batch_size)).astype(np.float32)
 
             out = fprop_function(x)
+            gm, gv = stats_function()
 
             xmean = x.mean(axis=1, keepdims=True)
             xvar = x.var(axis=1, keepdims=True)
-            out_ref = (x - xmean) / np.sqrt(xvar + eps)
+            out_ref = (x - xmean) / np.sqrt(xvar + epsilon)
             gmean_ref = xmean.ravel() * (1.0 - rho) + gmean_ref * rho
             gvar_ref = xvar.ravel() * (1.0 - rho) + gvar_ref * rho
-
-            # TODO: use value_op for this type of retrieval instead
-            gm = layer.gmean.value.get(None)
-            gv = layer.gvar.value.get(None)
 
             assert ng.testing.allclose(out,
                                        out_ref, atol=1e-6), '%e' % np.max(np.abs(out - out_ref))
