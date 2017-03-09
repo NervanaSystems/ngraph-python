0fe9eba3ca94233f9ed0debb61bbfc83040a4e25
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2017-03-09 10:47:34 -0800

    anahita/unit tests and fixes for hetr_utils (#1124)
    * Adding unit tests for hetr_utils
    
    * Add missing header for files
    
    * Handled code review f/b except scatter/gather handling
    
    * Code review feedback on 3-1 and 3-2
    
    * Code review feedback, debugging update_comm_deps for scatter-gather nodes
    
    * Code review feedback,added scatter-gather tests for update_comm_deps() and fixed issue in comm_path_exists()
    
    * Reversed send_nodes as list, cleaned up code
    
    * some more cleanup
    
    * cleanup
    
    * zach's review feedback
    
    * rename method in test_comm_nodes

--- ngraph/util/hetr_utils.py
+++ ngraph/util/hetr_utils.py
@@ -1,3 +1,17 @@
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 from __future__ import division
 from ngraph.op_graph.op_graph import AssignableTensorOp, BroadcastOp, \
     TensorValueOp
@@ -89,6 +103,7 @@ def comm_path_exists(fro, to):
     # from fro to to?
 
     visit = OrderedSet(fro.args)
+    visit.add(fro)
     while visit:
         v = visit.pop()
         if v == to:
@@ -118,7 +133,7 @@ def find_recvs(fro):
     return recvs
 
 
-def sort_ops_by_comm_deps(ops):
+def update_comm_deps(ops):
     """
     Sort the subgraphs identified by ops using communication dependencies.
 
---
+++ tests/test_comm_nodes.py
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------
+# Copyright 2017 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+from ngraph.factory.comm_nodes import calculate_new_axes
+import ngraph as ng
+import pytest
+
+ax_A = ng.make_axis(length=10, name='A')
+ax_B = ng.make_axis(length=15, name='B')
+ax_C = ng.make_axis(length=20, name='C')
+axes = ng.make_axes([ax_A, ax_B, ax_C])
+
+
+@pytest.mark.parametrize("is_last", [False, True])
+def test_calculate_new_axes_single_device(is_last):
+    new_axes = calculate_new_axes(axes=axes, parallel_axis=ax_B, num_devices=1, is_last=is_last)
+    assert new_axes.full_lengths == axes.full_lengths
+
+
+@pytest.mark.parametrize("is_last, expected_B_length", [(False, 7), (True, 8)])
+def tests_calculate_new_axes_two_devices(is_last, expected_B_length):
+    new_axes = calculate_new_axes(axes=axes, parallel_axis=ax_B, num_devices=2, is_last=is_last)
+    expected_axes = ng.make_axes([ax_A, ng.make_axis(length=expected_B_length, name='B'), ax_C])
+    assert new_axes.full_lengths == expected_axes.full_lengths
+
+
+def test_calculate_new_axes_zero_devices():
+    with pytest.raises(ZeroDivisionError):
+        calculate_new_axes(axes=axes, parallel_axis=ax_B, num_devices=0, is_last=False)
+
+
+def test_calculate_new_axes_null_axes():
+    with pytest.raises(TypeError):
+        calculate_new_axes(axes=None, parallel_axis=ax_B, num_devices=2, is_last=True)
+
+
+def test_calculate_new_axes_null_parallel_axis():
+    new_axes = calculate_new_axes(axes=axes, parallel_axis=None, num_devices=1, is_last=False)
+    # Checks null parallel axis. The axes calculated should have the same length as original
+    assert new_axes.full_lengths == axes.full_lengths
