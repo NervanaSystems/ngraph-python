1e8fcc146c61974f3fb37095dc0f531bbdedf9c4
Alex Park <alex.park@gmail.com>
2017-03-28 12:31:22 -0700

    Change aeon install setup to use cmake-based rc1-master Add option for cifar resnet to use aeon-based dataloader (needed for augmentations)

---
+++ ngraph/frontends/neon/aeon_shim.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+from __future__ import print_function
+import ngraph as ng
+from ngraph.frontends.neon import ax
+
+try:
+    from aeon import Dataloader
+except ImportError:
+    print(
+        "\n"
+        "Unable to import aeon module.\n"
+        "Please see installation instructions at:\n"
+        "*****************\n"
+        "https://github.com/NervanaSystems/aeon/blob/rc1-master/README.md\n"
+        "*****************\n"
+    )
+    import sys
+    sys.exit(1)
+
+
+class AeonDataloader(Dataloader):
+    def make_placeholders(self, include_iteration=False):
+        placeholders = {}
+        ax.N.length = self.batch_size
+        for placeholder_name, axis_info in self.axes_info.items():
+            p_axes = ng.make_axes([ax.N])
+            for nm, sz in axis_info.items():
+                nm = "C" if nm == "channels" else nm
+                p_axes += ng.make_axis(name=nm, length=sz)
+            placeholders[placeholder_name] = ng.placeholder(p_axes)
+        if include_iteration:
+            placeholders['iteration'] = ng.placeholder(axes=())
+        return placeholders
