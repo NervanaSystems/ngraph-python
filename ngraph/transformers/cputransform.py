35b4a4575704492dbeda4ff0a975041b8b208846
Wei Wang <wei.v.wang@intel.com>
2017-03-27 17:05:34 -0700

    Try to Use update function to fix issue #1246

--- ngraph/transformers/cputransform.py
+++ ngraph/transformers/cputransform.py
@@ -672,6 +672,7 @@ class CPUCodeGenerator(PyGen):
     def generate_op(self, op, out, *args):
         recv_id = len(self.recv_nodes)
         self.recv_nodes[recv_id] = op
+        # Use update function rather than if-else to handle scalar and array/slicing
         self.append("self.update_a_{}(self.recv_from_queue_send({}))",
                     out.tensor_description.name, recv_id)
 
