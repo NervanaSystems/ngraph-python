01ff9b0c8b65fd31e1ce84b6d08111fedd9352ee
Neuroschemata <anthony@sv3-0107-07.sv3.cloud.nervanasys.com>
2017-04-11 16:42:43 -0700

    update ctc header

--- third_party/warp_ctc/include_header.py
+++ third_party/warp_ctc/include_header.py
@@ -12,33 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------
-
-header = """
-typedef struct {
-int loc; 
-unsigned int num_threads;
-void* stream;
-int blank_label;
-}ctcOptions;
-
-int get_workspace_size(const int* const label_lengths,
-                       const int* const input_lengths,
-                       int alphabet_size, 
-                       int minibatch,
-                       ctcOptions options,
-                       size_t* size_bytes);
-
-int compute_ctc_loss(const float* const activations,
-                     float* gradients,
-                     const int* const flat_labels,
-                     const int* const label_lengths,
-                     const int* const input_lengths,
-                     int alphabet_size,
-                     int minibatch,
-                     float *costs,
-                     void *workspace, 
-                     ctcOptions options);
-"""
-
 def ctc_header():
+    header = """
+    typedef struct {
+    int loc; 
+    unsigned int num_threads;
+    void* stream;
+    int blank_label;
+    }ctcOptions;
+
+    int get_workspace_size(const int* const label_lengths,
+                           const int* const input_lengths,
+                           int alphabet_size, 
+                           int minibatch,
+                           ctcOptions options,
+                           size_t* size_bytes);
+
+    int compute_ctc_loss(const float* const activations,
+                         float* gradients,
+                         const int* const flat_labels,
+                         const int* const label_lengths,
+                         const int* const input_lengths,
+                         int alphabet_size,
+                         int minibatch,
+                         float *costs,
+                         void *workspace, 
+                         ctcOptions options);
+    """
     return header
