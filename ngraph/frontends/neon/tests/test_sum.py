8dc2bb165a0397a56d604cb96df022b7c63fecd4
Tyler Lee <tyler@nervanasys.com>
2017-05-05 16:46:37 -0700

    Clean-up test

--- ngraph/frontends/neon/tests/test_sum.py
+++ ngraph/frontends/neon/tests/test_sum.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 import ngraph as ng
+from ngraph.testing import executor, assert_allclose
 
 
 np.random.seed(0)
@@ -26,23 +27,16 @@ def extra_axes(request):
     return request.param
 
 
-def test_sum(transformer_factory, num_units, sequence_length, batch_size, extra_axes):
+def test_sum(transformer_factory, num_units, sequence_length, batch_size):
     """
-    There is a non-deterministic error in ng.sum with the gpu transformer. The test below
-    should show it.
-    :param transformer_factory:
-    :return:
+    This tests for a non-deterministic error that arose in ng.sum following a dot product using the gpu transformer.
     """
 
-    # Mimic the output of a convolutional layer (if extra_axes > 0)
-    # This doesn't seem to matter like I thought it did.
-    shape = tuple([num_units] + [1] * extra_axes + [sequence_length, batch_size])
+    shape = (num_units, sequence_length, batch_size)
     np_inp = np.random.uniform(-1, 1, shape)
 
     # Use an identity weight matrix on top of it
     np_w = np.eye(shape[0])
-    for _ in range(len(shape) - 3):
-        np_w = np.expand_dims(np_w, 2)
 
     # Create ngraph versions
     inp = ng.constant(np_inp)
@@ -66,13 +60,8 @@ def test_sum(transformer_factory, num_units, sequence_length, batch_size, extra_
     y = ng.sum(x, out_axes=output_axes)
     np_y = np.sum(np_x, axis=1)
 
-    t = transformer_factory()
-    f = t.computation([y, x])
-    y_val, x_val = f()
+    with executor([y, x]) as f:
+        y_val, x_val = f()
 
-    assert np.allclose(x_val.ravel(),
-                       np_x.ravel(),
-                       atol=1e-5), "Max difference: {}".format(np.max(np.abs(x_val - np_x)))
-    assert np.allclose(y_val,
-                       np_y,
-                       atol=1e-5), "Max difference: {}".format(np.max(np.abs(y_val - np_y)))
+        assert_allclose(x_val.ravel(), np_x.ravel(), atol=1e-5)
+        assert_allclose(y_val, np_y, atol=1e-5)
