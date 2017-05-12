000dca8709838cf57f653f2e4531e50135fc6b2c
Tyler Lee <tyler@nervanasys.com>
2017-05-12 14:00:16 -0700

    Added additional tests for layer metadata

--- ngraph/frontends/neon/tests/test_layer_subgraph.py
+++ ngraph/frontends/neon/tests/test_layer_subgraph.py
@@ -20,6 +20,18 @@ class SimpleLayer(Layer):
                               self.weight * in_obj])
 
 
+class NestedLayer(Layer):
+
+    def __init__(self):
+        super(NestedLayer, self).__init__()
+        self.inner_layer = SimpleLayer()
+
+    @neon_layer()
+    def __call__(self, in_obj):
+
+        return self.inner_layer(in_obj)
+
+
 @pytest.fixture(params=[32])
 def batch_size(request):
     return request.param
@@ -31,7 +43,6 @@ def input_size(request):
 
 
 def test_layer_inputs(input_placeholder):
-
     layer = SimpleLayer()
     assert layer.inputs is None
 
@@ -59,3 +70,26 @@ def test_layer_side_effects(input_placeholder):
 
     assert len(layer.side_effects) == 1
     assert isinstance(layer.side_effects[0], ng.AssignOp)
+
+
+def test_layer_metadata(input_placeholder):
+    layer = SimpleLayer()
+    layer(input_placeholder)
+
+    for op in layer.ops[0]:
+        assert "neon_layer" in op.metadata
+        assert len(op.metadata["neon_layer"]) == 1
+        assert op.metadata["neon_layer"] == ["simplelayer"]
+
+    assert "label" in layer.variables[0].metadata
+    assert layer.variables[0].metadata["label"] == LABELS["weight"]
+
+
+def test_nested_layer_metadata(input_placeholder):
+    layer = NestedLayer()
+    layer(input_placeholder)
+
+    for op in layer.ops[0]:
+        assert "neon_layer" in op.metadata
+        assert len(op.metadata["neon_layer"]) == 2
+        assert op.metadata["neon_layer"] == ["nestedlayer", "simplelayer"]
