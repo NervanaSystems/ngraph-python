71139366c6bc43fbf69ab2bbaf66a4d9d8432cdc
Jayaram Bobba <jayaram.bobba@intel.com>
2017-05-12 20:34:55 -0700

    Removed old MKL init functions

--- ngraph/transformers/cpu/cpuengine.py
+++ ngraph/transformers/cpu/cpuengine.py
@@ -228,32 +228,6 @@ class Mkldnn(object):
                 slicedI = E[:, sliceD, sliceH, sliceW, :].reshape((-1, N))
                 gI[:, m, p, q, :] = np.dot(slicedF.T, slicedI)
 
-    def init_pool_fprop(self, pool_type, name, arrI, arrO, kernel, pad, stride):
-        if (self.mkldnn_enabled):
-            C, D, H, W, N = arrI.shape
-            [J, T, R, S] = kernel
-            # Only 2D pooling supported in MKLDNN for now
-            if (D != 1 or T != 1 or J != 1):
-                return
-            # Only single precision float supported for now
-            if ((arrI.dtype != np.float32) or (arrO.dtype != np.float32)):
-                return
-            # Sanity check tensor shapes
-            if ((len(arrI.shape) != 5) or (len(arrO.shape) != 5) or
-               (len(stride) != 3) or (len(pad) != 3)):
-                return
-            input_shape = ((ctypes.c_int) * len(arrI.shape))(*arrI.shape)
-            output_shape = ((ctypes.c_int) * len(arrO.shape))(*arrO.shape)
-            kernel_sizes = ((ctypes.c_int) * len(kernel))(*kernel)
-            pad_data = ((ctypes.c_int) * len(pad))(*pad)
-            stride_data = ((ctypes.c_int) * len(stride))(*stride)
-            self.create_mkldnn_pool_fprop_primitives_fn(
-                self.mkldnn_engine,
-                len(arrI.shape), len(arrO.shape), len(stride), len(pad),
-                input_shape, kernel_sizes, output_shape,
-                arrI.ctypes.data, arrO.ctypes.data,
-                stride_data, pad_data, pool_type, self.kernels[name])
-
     def fprop_pool(self, name, pool_slices, arrI, arrO):
         if (self.mkldnn_enabled and name in self.kernels):
             kSlice, mSlice, pSlice, qSlice, op, arrA = pool_slices
@@ -282,29 +256,6 @@ class Mkldnn(object):
                     arrO[k, m, p, q, :] = np.mean(sliceI, axis=0)
                 elif op == "l2":
                     arrO[k, m, p, q, :] = np.sqrt(np.sum(np.square(sliceI), axis=0))
-
-    def init_pool_bprop(self, pool_type, name, fprop_name, arrE, arrD, kernel, pad, stride):
-        if (self.mkldnn_enabled):
-            C, D, H, W, N = arrE.shape
-            [J, T, R, S] = kernel
-            # Only 2D pooling supported in MKLDNN for now
-            if (D != 1 or T != 1 or J != 1):
-                return
-            # Only single precision float supported for now
-            if ((arrE.dtype != np.float32) or (arrD.dtype != np.float32)):
-                return
-            input_shape = ((ctypes.c_int) * len(arrE.shape))(*arrE.shape)
-            output_shape = ((ctypes.c_int) * len(arrD.shape))(*arrD.shape)
-            kernel_sizes = ((ctypes.c_int) * len(kernel))(*kernel)
-            pad_data = ((ctypes.c_int) * len(pad))(*pad)
-            stride_data = ((ctypes.c_int) * len(stride))(*stride)
-            self.kernels[name] = \
-                self.create_mkldnn_pool_bprop_primitives_fn(
-                    self.mkldnn_engine,
-                    len(arrE.shape), len(arrD.shape), len(stride), len(pad),
-                    input_shape, kernel_sizes, output_shape,
-                    arrE.ctypes.data, arrD.ctypes.data,
-                    stride_data, pad_data, pool_type, self.kernels[fprop_name])
 
     def bprop_pool(self, name, pool_slices, arrE, arrD):
         if (self.mkldnn_enabled and name in self.kernels):
@@ -390,20 +341,6 @@ class Mkldnn(object):
             self.run_mkldnn_netlist_fn(self.kernels[name])
         else:
             np.add(I_array1, I_array2, out=O_array)
-
-    def init_relu_fprop(self, name, inputs, out, slope):
-        if (self.mkldnn_enabled):
-            if (self.mkldnn_verbose):
-                print("Relu Input: ", len(inputs.shape), inputs.shape,
-                      " Outputs: ", out.shape, len(out.shape))
-            # Only single precision float supported for now
-            if ((inputs.dtype != np.float32) or (out.dtype != np.float32)):
-                return
-            input_size = np.prod(inputs.shape)
-            self.kernels[name] = \
-                self.create_mkldnn_relu_fprop_primitives_fn(
-                    self.mkldnn_engine, inputs.ctypes.data, out.ctypes.data,
-                    slope, input_size)
 
     def fprop_relu(self, name, inputs, out, slope):
         if (self.mkldnn_enabled and name in self.kernels):
