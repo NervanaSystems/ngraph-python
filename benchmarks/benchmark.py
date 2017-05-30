2d80fbea9b59886a1d61b7a15ca6bdbda9f116ab
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2017-05-30 09:46:03 -0700

    Adding device with device keyword

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -81,7 +81,7 @@ class Benchmark(object):
         feed_dict = self.fill_feed_dict(self.train_set, self.inputs)
         start = Benchmark.marker.init_mark()
         end = Benchmark.marker.init_mark()
-        with closing(ngt.make_transformer_factory(self.transformer)()) as transformer:
+        with closing(ngt.make_transformer_factory(self.transformer, device=self.device)()) as transformer:
             if visualize:
                 nviz = ngraph.transformers.passes.nviz.VizPass(show_axes=True,
                                                                show_all_metadata=True)
