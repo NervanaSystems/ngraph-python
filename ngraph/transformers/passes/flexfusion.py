169bc04d68e530285bfad895bfa08a348753b8c3
ppieniaz <pawel.pieniazek@intel.com>
2017-06-01 13:57:45 +0200

    fixes after review

--- ngraph/transformers/passes/flexfusion.py
+++ ngraph/transformers/passes/flexfusion.py
@@ -12,12 +12,9 @@ class FlexFusion(GraphRewritePass):
 
         Returns:
             Single pattern that matches Sigmoid
-
         """
 
-        # We want to match x tensor for Sigmoid.
         self.sigmoid_x_label = "X"
-        # We bind op to X unconditionally.
         x = PatternLabelOp(self.sigmoid_x_label, axes={ng.make_axis(name='N')})
         sigmoid_op = ng.sigmoid(x)
         return sigmoid_op
@@ -32,17 +29,15 @@ class FlexFusion(GraphRewritePass):
             sigmoid_atomic_op = ng.sigmoidAtomic(x)
             self.replace_op(op, sigmoid_atomic_op)
 
-    def construct_ce_binnary_inner_pattern(self):
+    def construct_ce_binary_inner_pattern(self):
         """
-        Generate graph op that represents a pattern for Cross Entropy Binnary Inner operation
+        Generate graph op that represents a pattern for Cross Entropy Binary Inner operation
         ng.cross_entropy_binary_inner(y, t, enable_sig_opt=True, enable_diff_opt=True)
 
         Returns:
-            Single pattern that matches Sigmoid
-
+            Single pattern that matches "optimized" Cross Entropy Binary Inner
         """
 
-        # We want to match x tensor for Sigmoid.
         self.ce_x_label = "X"
         self.ce_y_label = "Y"
         self.ce_t_label = "T"
@@ -56,9 +51,9 @@ class FlexFusion(GraphRewritePass):
         cross_op = ng.cross_entropy_binary_inner(y, t, enable_sig_opt=True, enable_diff_opt=True)
         return cross_op
 
-    def fuse_ce_binnary_inner_callback(self, op, label_map_op_list):
+    def fuse_ce_binary_inner_callback(self, op, label_map_op_list):
         """
-        Callback function that handles fusion for cross_entropy_binnary_inner pattern
+        Callback function that handles fusion for cross_entropy_binary_inner pattern
         """
         for (label_map, op) in label_map_op_list:
             # Matched Sigmoid pattern, do the replacement here.
@@ -74,6 +69,6 @@ class FlexFusion(GraphRewritePass):
         pattern_sigmoid = self.construct_sigmoid_pattern()
         self.register_pattern(pattern_sigmoid, self.fuse_sigmoid_callback)
 
-        # Register cross_entropy_binnary_inner pattern
-        pattern_ce_binnary_inner = self.construct_ce_binnary_inner_pattern()
-        self.register_pattern(pattern_ce_binnary_inner, self.fuse_ce_binnary_inner_callback)
+        # Register cross_entropy_binary_inner pattern
+        pattern_ce_binary_inner = self.construct_ce_binary_inner_pattern()
+        self.register_pattern(pattern_ce_binary_inner, self.fuse_ce_binary_inner_callback)
