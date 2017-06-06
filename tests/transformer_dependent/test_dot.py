Flex - Fixed symbolic and numeric derivatives tests

tests/transformer_dependent/test_dot.py
@@ -118,20 +118,25 @@ def test_dot_sum_backprop(transformer_factory):
     d = ng.dot(x, y)
     s = ng.sum(d, out_axes=())
 
+    x_cpu = ng.placeholder(x_axes)
+    y_cpu = ng.placeholder(y_axes)
+    d_cpu = ng.dot(x_cpu, y_cpu)
+    s_cpu = ng.sum(d_cpu, out_axes=())
+
     with ExecutorFactory() as ex:
         s_fun = ex.executor(s, x, y)
         d_fun = ex.executor(d, x, y)
 
-        dd_dx_fun_num = ex.numeric_derivative(d, x, delta, y)
+        dd_dx_fun_num = ex.numeric_derivative(d_cpu, x_cpu, delta, y_cpu)
         dd_dx_fun_sym = ex.derivative(d, x, y)
 
-        dd_dy_fun_num = ex.numeric_derivative(d, y, delta, x)
+        dd_dy_fun_num = ex.numeric_derivative(d_cpu, y_cpu, delta, x_cpu)
         dd_dy_fun_sym = ex.derivative(d, y, x)
 
-        ds_dx_fun_num = ex.numeric_derivative(s, x, delta, y)
+        ds_dx_fun_num = ex.numeric_derivative(s_cpu, x_cpu, delta, y_cpu)
         ds_dx_fun_sym = ex.derivative(s, x, y)
 
-        ds_dy_fun_num = ex.numeric_derivative(s, y, delta, x)
+        ds_dy_fun_num = ex.numeric_derivative(s_cpu, y_cpu, delta, x_cpu)
         ds_dy_fun_sym = ex.derivative(s, y, x)
 
         # assert outputs are equal
@@ -139,30 +144,29 @@ def test_dot_sum_backprop(transformer_factory):
         d_val = d_fun(x_np, y_np)
         ng.testing.assert_allclose(d_np, d_val, rtol=rtol, atol=atol)
 
-        dd_dx_val_num = dd_dx_fun_num(x_np, y_np)
-        dd_dx_val_sym = dd_dx_fun_sym(x_np, y_np)
-        ng.testing.assert_allclose(dd_dx_val_num, dd_dx_val_sym, rtol=rtol, atol=atol)
-
-        dd_dy_val_num = dd_dy_fun_num(y_np, x_np)
-        dd_dy_val_sym = dd_dy_fun_sym(y_np, x_np)
-        ng.testing.assert_allclose(dd_dy_val_num, dd_dy_val_sym, rtol=rtol, atol=atol)
-
         s_np = np.sum(d_np)
         s_val = s_fun(x_np, y_np)
         ng.testing.assert_allclose(s_val, s_np, rtol=rtol, atol=atol)
 
         # assert derivative wrt to both tensors is the same when computed
         # symbolically by ngraph and numerically
-        ds_dx_val_num = ds_dx_fun_num(x_np, y_np)
-        ds_dx_val_sym = ds_dx_fun_sym(x_np, y_np)
-        ng.testing.assert_allclose(ds_dx_val_num, ds_dx_val_sym, rtol=rtol, atol=atol)
 
-        ds_dy_val_num = ds_dy_fun_num(y_np, x_np)
+        dd_dx_val_sym = dd_dx_fun_sym(x_np, y_np)
+        dd_dy_val_sym = dd_dy_fun_sym(y_np, x_np)
+        ds_dx_val_sym = ds_dx_fun_sym(x_np, y_np)
         ds_dy_val_sym = ds_dy_fun_sym(y_np, x_np)
+
+        dd_dx_val_num = dd_dx_fun_num(x_np, y_np)
+        dd_dy_val_num = dd_dy_fun_num(y_np, x_np)
+        ds_dx_val_num = ds_dx_fun_num(x_np, y_np)
+        ds_dy_val_num = ds_dy_fun_num(y_np, x_np)
+
+        ng.testing.assert_allclose(dd_dx_val_num, dd_dx_val_sym, rtol=rtol, atol=atol)
+        ng.testing.assert_allclose(dd_dy_val_num, dd_dy_val_sym, rtol=rtol, atol=atol)
+        ng.testing.assert_allclose(ds_dx_val_num, ds_dx_val_sym, rtol=rtol, atol=atol)
         ng.testing.assert_allclose(ds_dy_val_num, ds_dy_val_sym, rtol=rtol, atol=atol)
 
 
-@pytest.mark.flex_disabled
 @raise_all_numpy_errors
 def test_tensor_dot_tensor(transformer_factory):
     """TODO."""
@@ -228,17 +232,22 @@ def test_tensor_dot_tensor(transformer_factory):
             test['tensor2'], dtype=np.float32
         )
 
+        tensor1_cpu = ng.placeholder(test['tensor1_axes'])
+        tensor2_cpu = ng.placeholder(test['tensor2_axes'])
+
         # compute outputs
         expected_output = np.array(test['expected_output'], dtype=np.float32)
 
         with ExecutorFactory() as ex:
             dot = ng.dot(tensor1, tensor2)
+            dot_cpu = ng.dot(tensor1_cpu, tensor2_cpu)
+
             evaluated_fun = ex.executor(dot, tensor1, tensor2)
 
-            deriv1_fun_num = ex.numeric_derivative(dot, tensor1, 1e-3, tensor2)
+            deriv1_fun_num = ex.numeric_derivative(dot_cpu, tensor1_cpu, 1e-3, tensor2_cpu)
             deriv1_fun_sym = ex.derivative(dot, tensor1, tensor2)
 
-            deriv2_fun_num = ex.numeric_derivative(dot, tensor2, 1e-3, tensor1)
+            deriv2_fun_num = ex.numeric_derivative(dot_cpu, tensor2_cpu, 1e-3, tensor1_cpu)
             deriv2_fun_sym = ex.derivative(dot, tensor2, tensor1)
 
             # assert outputs are equal
@@ -247,12 +256,13 @@ def test_tensor_dot_tensor(transformer_factory):
 
             # assert derivative wrt to both tensors is the same when computed
             # symbolically by ngraph and numerically
-            deriv1_val_num = deriv1_fun_num(value1, value2)
             deriv1_val_sym = deriv1_fun_sym(value1, value2)
-            ng.testing.assert_allclose(deriv1_val_num, deriv1_val_sym, rtol=1e-2, atol=1e-2)
-
-            deriv2_val_num = deriv2_fun_num(value2, value1)
             deriv2_val_sym = deriv2_fun_sym(value2, value1)
+
+            deriv1_val_num = deriv1_fun_num(value1, value2)
+            deriv2_val_num = deriv2_fun_num(value2, value1)
+
+            ng.testing.assert_allclose(deriv1_val_num, deriv1_val_sym, rtol=1e-2, atol=1e-2)
             ng.testing.assert_allclose(deriv2_val_num, deriv2_val_sym, rtol=1e-2, atol=1e-2)
 
 

tests/transformer_dependent/test_execution.py
@@ -14,6 +14,7 @@
 # ----------------------------------------------------------------------------
 from __future__ import division
 from builtins import range
+from copy import deepcopy
 
 import numpy as np
 import pytest
@@ -201,7 +202,6 @@ def test_reduction_deriv(transformer_factory, reduction, sub_axes):
     check_derivative(graph_reduce, p_u, delta, u, atol=1e-1, rtol=1e-1)
 
 
-@pytest.mark.flex_disabled
 def test_prod_constant(transformer_factory):
     """
     Test reduce product of constants
@@ -246,7 +246,6 @@ def test_prod_constant(transformer_factory):
     np.testing.assert_allclose(res_0_1_2_np, res_0_1_2_ng)
 
 
-@pytest.mark.flex_disabled
 def test_prod_deriv(transformer_factory):
     """
     Test reduce product's gradient
@@ -311,7 +310,6 @@ def test_prod_deriv(transformer_factory):
             check_derivative(x_prod, x, 0.001, x_val, atol=1e-3, rtol=1e-3)
 
 
-@pytest.mark.flex_disabled
 def test_reciprocal(transformer_factory, input_tensor):
     """TODO."""
     p_u = input_tensor
@@ -325,7 +323,6 @@ def test_reciprocal(transformer_factory, input_tensor):
     ng.testing.assert_allclose(rec_u_np, rec_u_graph)
 
 
-@pytest.mark.flex_disabled
 def test_reciprocal_derivative(transformer_factory, input_tensor):
     """TODO."""
     p_u = input_tensor
@@ -408,7 +405,6 @@ def test_elementwise_binary_ops_matched_args(
     )
 
 
-@pytest.mark.flex_disabled
 def test_elementwise_binary_ops_matched_args_deriv_lhs(
     transformer_factory,
     elementwise_binary_op,
@@ -430,7 +426,6 @@ def test_elementwise_binary_ops_matched_args_deriv_lhs(
     )
 
 
-@pytest.mark.flex_disabled
 def test_elementwise_binary_ops_matched_args_deriv_rhs(
     transformer_factory,
     elementwise_binary_op,
@@ -452,7 +447,6 @@ def test_elementwise_binary_ops_matched_args_deriv_rhs(
     )
 
 
-@pytest.mark.flex_disabled
 def test_elementwise_unary_ops_matched_args(
     transformer_factory,
     elementwise_unary_op,
@@ -464,19 +458,21 @@ def test_elementwise_unary_ops_matched_args(
     be_op = getattr(ng, elementwise_unary_op)
 
     p_u = symmetric_tensor
+    p_u_cpu = deepcopy(symmetric_tensor)
     u = rng.uniform(1.0, 2.0, p_u.axes)
     u_np = np_op(u)
     result_op = be_op(p_u)
+    result_op_cpu = be_op(p_u_cpu)
 
     with ExecutorFactory() as ex:
         fun = ex.executor(result_op, p_u)
-        dudunum_fun = ex.numeric_derivative(result_op, p_u, delta)
+        dudunum_fun = ex.numeric_derivative(result_op_cpu, p_u_cpu, delta)
         dudut_fun = ex.derivative(result_op, p_u)
 
         u_t = fun(u)
         ng.testing.assert_allclose(u_np, u_t, atol=1e-4, rtol=1e-4)
-        dudunum = dudunum_fun(u)
         dudut = dudut_fun(u)
+        dudunum = dudunum_fun(u)
         ng.testing.assert_allclose(dudunum, dudut, atol=1e-3, rtol=1e-3)
 
 
@@ -617,7 +613,6 @@ def test_cross_entropy_binary_logistic_shortcut(
     ng.testing.assert_allclose(cel, cel_graph, rtol=1e-5)
 
 
-@pytest.mark.flex_disabled
 def test_cross_entropy_binary(
     transformer_factory,
     input_tensor
@@ -900,7 +895,6 @@ def test_cross_entropy_multi_axis_order(transformer_factory, input_tensor):
         ng.testing.assert_allclose(out1.ravel(), out2.ravel(), rtol=1e-5)
 
 
-@pytest.mark.flex_disabled
 def test_sigmoid_deriv(transformer_factory, input_tensor):
     """TODO."""
     p_u = input_tensor
@@ -911,7 +905,6 @@ def test_sigmoid_deriv(transformer_factory, input_tensor):
     check_derivative(val_u, p_u, 0.001, u, atol=1e-2, rtol=1e-2)
 
 
-@pytest.mark.flex_disabled
 def test_log_sigmoid_deriv(transformer_factory, input_tensor):
     """TODO."""
     p_u = input_tensor

tests/transformer_dependent/test_reshaping.py
@@ -65,11 +65,14 @@ def test_expand_dims(transformer_factory):
                     test['tensor'], dtype=np.float32
                 )
                 tensor = ng.placeholder(tensor_axes)
+                tensor_cpu = ng.placeholder(tensor_axes)
 
                 expanded = ng.ExpandDims(tensor, new_axis, dim)
+                expanded_cpu = ng.ExpandDims(tensor_cpu, new_axis, dim)
+
                 with ExecutorFactory() as ex:
                     expander_fun = ex.executor(expanded, tensor)
-                    num_deriv_fun = ex.numeric_derivative(expanded, tensor, delta)
+                    num_deriv_fun = ex.numeric_derivative(expanded_cpu, tensor_cpu, delta)
                     sym_deriv_fun = ex.derivative(expanded, tensor)
 
                     expanded_shape = tensor_np.shape[:dim] \