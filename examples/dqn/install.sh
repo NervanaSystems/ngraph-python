c35d00e33124cd8f4b757201ce22091d5cc0a1d7
Zach Dwiel <zach@nervanasys.com>
2017-06-08 07:35:32 -0700

    update install script

--- examples/dqn/install.sh
+++ examples/dqn/install.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -y cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
 
-pip install requirements.txt
+pip install -r requirements.txt
