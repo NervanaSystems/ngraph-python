bff9bf0aa523cca7b96933109175c7b481dbff29
Gokce Keskin <gkeskin@gmail.com>
2017-07-14 13:25:52 -0700

    Create Branch Text Generation Example

---
+++ examples/text_gen/rollingWindowIterator.py
@@ -0,0 +1,175 @@
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+import numpy as np
+import ngraph as ng
+from future.utils import viewitems
+import six
+from ngraph.frontends.neon import ax
+import collections
+
+
+class RollingWindowIterator(object):
+
+    def __init__(self, data_arrays, batch_size, seq_len, total_iterations=None):
+        """
+        Takes in a dict, where each item is a sequence. 
+        For each item, generates samples that are overlapping rolling windows of the sequence.
+        Each iteration returns 
+            input X of size (batch_size, seq_len, input_feature_dim) 
+            ground truth output y of size (batch_size, 1, output_feature_dim)
+        Example:
+            data_arrays['seq1'] is an array with shape (N,): [a1, a2, ..., aN]
+            Each generated sample will be an input sequence / output single point pairs such as:
+                Input:  [a1, a2, ..., a(seq_len)], Output: a(seq_len + 1)
+                        [a2, a3, ..., a(seq_len+1)], Output: a(seq_len +2)
+                        ...
+             
+        During initialization, the input data will be converted to backend tensor objects
+        (e.g. CPUTensor or GPUTensor). If the backend uses the GPU, the data is copied over to the
+        device.
+
+        Args:
+            data_arrays (dict) : input data
+                data_arrays['X'] = array of shape (total_sequence_length,)
+                data_arrays['y'] = array of shape (total_sequence_length,)
+            batch_size (int): number of examples in each minibatch
+            seq_len (int): number of timesteps in each input sample
+            total_iterations (int): number of minibatches to cycle through on this iterator.
+                                    If not provided, it will cycle through all of the data once.
+        """
+
+        # Treat singletons like list so that iteration follows same syntax
+        self.batch_size = batch_size
+        self.keys = list(self.data_arrays.keys())
+
+        # Get the total length of the sequence
+        self.ndata = len(self.data_arrays[self.keys[0]])
+
+        if self.ndata < self.batch_size:
+            raise ValueError('Number of examples is smaller than the batch size')
+
+        self.start = 0
+        self.index = 0
+
+        self.total_iterations = self.nbatches if total_iterations is None else total_iterations
+
+    @property
+    def nbatches(self):
+        """
+        Return the number of minibatches in this dataset.
+        """
+        return -((self.start - self.ndata) // self.batch_size)
+
+    def make_placeholders(self, include_iteration=False):
+        placeholders = {}
+        ax.N.length = self.batch_size
+        for k, axnm in self.axis_names.items():
+            p_axes = ng.make_axes([ax.N])
+            for i, sz in enumerate(self.data_arrays[k].shape[1:], 1):
+                name = axnm[i] if axnm else None
+                p_axes += ng.make_axis(length=sz, name=name)
+            placeholders[k] = ng.placeholder(p_axes)
+        if include_iteration:
+            placeholders['iteration'] = ng.placeholder(axes=())
+        return placeholders
+
+    def reset(self):
+        """
+        Resets the starting index of this dataset to zero. Useful for calling
+        repeated evaluations on the dataset without having to wrap around
+        the last uneven minibatch. Not necessary when data is divisible by batch size
+        """
+        self.start = 0
+        self.index = 0
+
+    def __iter__(self):
+        """
+        Returns a new minibatch of data with each call.
+
+        Yields:
+            tuple: The next minibatch which includes both features and labels.
+        """
+        i1 = self.start
+        while self.index < self.total_iterations:
+            #i1 = (self.start + self.index * self.batch_size) % self.ndata
+            strt_idx = (self.start + self.index * self.batch_size) 
+            end_idx = strt_idx + self.seq_len + self.batch_size
+            idcs = np.arange(strt_idx, end_idx) % self.ndata
+            self.index += 1
+            for key, val in self.data_arrays.items():
+                seq = np.take(val, idcs)
+                                
+            batch_bufs = {k: np.take( for k, src in self.data_arrays.items()}
+
+            batch_bufs['iteration'] = self.index
+            yield batch_bufs
+
+        self.start = (self.start + self.total_iterations * self.batch_size) % self.ndata
+
+
+class SequentialArrayIterator(object):
+
+    def __init__(self, data_arrays, time_steps, batch_size,
+                 total_iterations=None, reverse_target=False, get_prev_target=False):
+        self.get_prev_target = get_prev_target
+        self.reverse_target = reverse_target
+
+        self.batch_size = batch_size
+        self.time_steps = time_steps
+        self.index = 0
+
+        if isinstance(data_arrays, dict):
+            self.data_arrays = {k: v for k, v in viewitems(data_arrays)}
+        else:
+            raise ValueError("Must provide dict as input")
+
+        # just get an arbitrary element for len
+        self.ndata = len(six.next(six.itervalues(self.data_arrays)))
+        self.ndata = self.ndata // (self.batch_size * self.time_steps) * self.batch_size
+        self.ntokens = self.ndata * self.time_steps
+        self.nbatches = self.ndata // self.batch_size
+
+        if self.ndata < self.batch_size:
+            raise ValueError('Number of examples is smaller than the batch size')
+
+        self.total_iterations = self.nbatches if total_iterations is None else total_iterations
+
+        self.data_arrays = {k: x[:self.ntokens].reshape(
+            self.batch_size,
+            self.nbatches,
+            self.time_steps
+        ) for k, x in viewitems(self.data_arrays)}
+
+        if self.reverse_target:
+            self.data_arrays['tgt_txt'][:] = self.data_arrays['tgt_txt'][:, :, ::-1]
+
+        if self.get_prev_target:
+            self.data_arrays['prev_tgt'] = np.roll(self.data_arrays['tgt_txt'], shift=1, axis=2)
+
+    def make_placeholders(self):
+        batch_axis = ng.make_axis(length=self.batch_size, name="N")
+        time_axis = ng.make_axis(length=self.time_steps, name="REC")
+        p_axes = ng.make_axes([batch_axis, time_axis])
+        return {k: ng.placeholder(p_axes) for k in self.data_arrays.keys()}
+
+    def reset(self):
+        self.index = 0
+
+    def __iter__(self):
+        while self.index < self.total_iterations:
+            idx = self.index % self.nbatches
+            self.index += 1
+
+            yield {k: np.squeeze(x[:, idx:(idx + 1), :]) for k, x in viewitems(self.data_arrays)}
---
+++ examples/text_gen/text_generation.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+from contextlib import closing
+import ngraph as ng
+from ngraph.frontends.neon import (Layer, Sequential, Preprocess, BiRNN, Recurrent, Affine,
+                                   Softmax, Tanh, LookupTable)
+from ngraph.frontends.neon import UniformInit, RMSProp
+from ngraph.frontends.neon import ax, loop_train
+from ngraph.frontends.neon import NgraphArgparser, make_bound_computation, make_default_callbacks
+from ngraph.frontends.neon import SequentialArrayIterator
+import ngraph.transformers as ngt
+
+from ngraph.frontends.neon import PTB
+
+
+# parse the command line arguments
+parser = NgraphArgparser(__doc__)
+parser.add_argument('--layer_type', default='rnn', choices=['rnn', 'birnn'],
+                    help='type of recurrent layer to use (rnn or birnn)')
+parser.add_argument('--use_lut', action='store_true',
+                    help='choose to use lut as first layer')
+parser.set_defaults()
+args = parser.parse_args()
+
+# these hyperparameters are from the paper
+args.batch_size = 50
+time_steps = 150
+hidden_size = 500
+
+# download penn treebank
+tree_bank_data = PTB(path=args.data_dir)
+ptb_data = tree_bank_data.load_data()
+train_set = SequentialArrayIterator(ptb_data['train'], batch_size=args.batch_size,
+                                    time_steps=time_steps, total_iterations=args.num_iterations)
+
+valid_set = SequentialArrayIterator(ptb_data['valid'], batch_size=args.batch_size,
+                                    time_steps=time_steps)
+
+inputs = train_set.make_placeholders()
+ax.Y.length = len(tree_bank_data.vocab)
+
+
+def expand_onehot(x):
+    return ng.one_hot(x, axis=ax.Y)
+
+
+# weight initialization
+init = UniformInit(low=-0.08, high=0.08)
+
+if args.use_lut:
+    layer_0 = LookupTable(50, 100, init, update=True, pad_idx=0)
+else:
+    layer_0 = Preprocess(functor=lambda x: ng.one_hot(x, axis=ax.Y))
+
+if args.layer_type == "rnn":
+    rlayer = Recurrent(hidden_size, init, activation=Tanh())
+elif args.layer_type == "birnn":
+    rlayer = BiRNN(hidden_size, init, activation=Tanh(), return_sequence=True, sum_out=True)
+
+if args.use_lut:
+    layer_0 = LookupTable(50, 100, init, update=False)
+else:
+    layer_0 = Preprocess(functor=expand_onehot)
+
+# model initialization
+seq1 = Sequential([layer_0,
+                   rlayer,
+                   Affine(init, activation=Softmax(), bias_init=init, axes=(ax.Y,))])
+
+optimizer = RMSProp()
+
+train_prob = seq1(inputs['inp_txt'])
+train_loss = ng.cross_entropy_multi(train_prob,
+                                    ng.one_hot(inputs['tgt_txt'], axis=ax.Y),
+                                    usebits=True)
+batch_cost = ng.sequential([optimizer(train_loss), ng.mean(train_loss, out_axes=())])
+train_outputs = dict(batch_cost=batch_cost)
+
+with Layer.inference_mode_on():
+    inference_prob = seq1(inputs['inp_txt'])
+eval_loss = ng.cross_entropy_multi(inference_prob,
+                                   ng.one_hot(inputs['tgt_txt'], axis=ax.Y),
+                                   usebits=True)
+eval_outputs = dict(cross_ent_loss=eval_loss)
+
+# Now bind the computations we are interested in
+with closing(ngt.make_transformer()) as transformer:
+    train_computation = make_bound_computation(transformer, train_outputs, inputs)
+    loss_computation = make_bound_computation(transformer, eval_outputs, inputs)
+
+    cbs = make_default_callbacks(transformer=transformer,
+                                 output_file=args.output_file,
+                                 frequency=args.iter_interval,
+                                 train_computation=train_computation,
+                                 total_iterations=args.num_iterations,
+                                 eval_set=valid_set,
+                                 loss_computation=loss_computation,
+                                 use_progress_bar=args.progress_bar)
+
+    loop_train(train_set, train_computation, cbs)
