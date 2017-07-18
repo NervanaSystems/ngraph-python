cb9e107bbe0b6cffe1098c4b764cd4b6d2e9bed5
Henry Chang <hengluchang@gmail.com>
2017-07-18 11:46:01 -0700

    create data loader for teacher_tgt

--- examples/ptr-net/tsp_seqarrayiter.py
+++ examples/ptr-net/tsp_seqarrayiter.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright 2016 Nervana Systems Inc.
+# Copyright 2017 Nervana Systems Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -21,7 +21,7 @@ import collections
 
 class TSPSequentialArrayIterator(object):
     """
-    modification of SequentialArrayIterator class in arrayiterator.py. 
+    modification of SequentialArrayIterator class.
     Add number of features argument to handle variable feature sizes.
 
     Args:
@@ -31,14 +31,10 @@ class TSPSequentialArrayIterator(object):
         n_features (int): nubmer of input features (dimensions) of each cities.
         total_iterations (int): number of minibatches to cycle through on this iterator.
                                 If not provided, it will cycle through all of the data once.
-        get_prev_target (bool): default to be True to use Teach Forcing (Williams and Zipser, 1989)
-                                during training.
     """
     def __init__(self, data_arrays, time_steps, batch_size, nfeatures,
-                 total_iterations=None, get_prev_target=True):
+                 total_iterations=None):
         self.nfeatures = nfeatures
-        self.get_prev_target = get_prev_target
-
         self.batch_size = batch_size
         self.time_steps = time_steps
         self.index = 0
@@ -62,27 +58,29 @@ class TSPSequentialArrayIterator(object):
 
         self.total_iterations = self.nbatches if total_iterations is None else total_iterations
 
-        # reshape array for placeholders
+        # reshape array for batch and batch size dimensions
         self.data_arrays['inp_txt'] = self.data_arrays['inp_txt'][:self.ndata][:][:].reshape(self.batch_size, self.nbatches, self.time_steps, self.nfeatures)
 
         self.data_arrays['tgt_txt'] = self.data_arrays['tgt_txt'][:self.ndata][:].reshape(self.batch_size, self.nbatches, self.time_steps)
 
+        self.data_arrays['teacher_tgt'] = self.data_arrays['teacher_tgt'][:self.ndata][:][:].reshape(self.batch_size, self.nbatches, self.time_steps, self.nfeatures)
+
         # Teacher Forcing
-        if self.get_prev_target:
-            self.data_arrays['prev_tgt'] = np.roll(self.data_arrays['tgt_txt'], shift=1, axis=2)
-            # put a start token (0 here) as the first input to the decoder
-            for i in range(self.batch_size):
-                for j in range(self.nbatches):
-                    np.put(self.data_arrays['prev_tgt'][i][j], [0], [0])
+        self.data_arrays['teacher_tgt'] = np.roll(self.data_arrays['teacher_tgt'], shift=1, axis=2)
+        # put a start token (0, 0) as the first decoder input
+        for i in range(self.batch_size):
+            for j in range(self.nbatches):
+                for k in range(self.nfeatures):
+                    np.put(self.data_arrays['teacher_tgt'][i][j][0], [k], [0])
 
     def make_placeholders(self):
         batch_axis = ng.make_axis(length=self.batch_size, name="N")
         time_axis = ng.make_axis(length=self.time_steps, name="REC")
-        feature_axis = ng.make_axis(length=self.nfeatures, name="feature_axis")
+        feature_axis = ng.make_axis(length=self.nfeatures, name="feature_axisjiji")
 
         dict = {}
         for k in self.data_arrays.keys():
-            if k == 'inp_txt':
+            if k == 'inp_txt' or k == 'teacher_tgt':
                 p_axes = ng.make_axes([batch_axis, time_axis, feature_axis])
             else:
                 p_axes = ng.make_axes([batch_axis, time_axis])
@@ -97,10 +95,9 @@ class TSPSequentialArrayIterator(object):
         while self.index < self.total_iterations:
             idx = self.index % self.nbatches
             self.index += 1
-
             dict = {}
             for k, x in viewitems(self.data_arrays):
-                if k == 'inp_txt':
+                if k == 'inp_txt' or k == 'teacher_tgt':
                     dict[k] = np.squeeze(x[:, idx:(idx + 1), :, :])
                 else:
                     dict[k] = np.squeeze(x[:, idx:(idx + 1), :])
