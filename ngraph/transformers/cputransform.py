5e842a550d896dee3b8e7deaf88bb5bc29bdab1c
Sang Ik Lee <sang.ik.lee@intel.com>
2017-07-26 15:47:24 -0700

    Problem: False unicode escape for mkldnn engine file path on Windows. Solution: Convert path to raw string.

--- ngraph/transformers/cputransform.py
+++ ngraph/transformers/cputransform.py
@@ -989,7 +989,7 @@ from ngraph.transformers.cpu.ctc import ctc_cpu
 
         mkldnn_path = os.path.join(os.path.dirname(__file__), "..", "..")
         mkldnn_engine_path = os.path.join(mkldnn_path, 'mkldnn_engine.so')
-        module.execute("mkldnn = Mkldnn('{}')".format(mkldnn_engine_path))
+        module.execute("mkldnn = Mkldnn(r'{}')".format(mkldnn_engine_path))
         module.execute("mkldnn.open()")
         self.mkldnn = module['mkldnn']
         if self.use_mlsl:
