c10563a767aa3c823b9db75052e664c992ad3d9d
rblaczkowski <rafal.blaczkowski@intel.com>
2017-08-02 15:29:34 +0200

    Add Licence notes and delete redundant prints

--- tests/transformer_dependent/test_serde_weights.py
+++ tests/transformer_dependent/test_serde_weights.py
@@ -1,5 +1,17 @@
-from __future__ import print_function
-
+# ----------------------------------------------------------------------------
+# Copyright 2017 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 import pytest
 import six
 from six import BytesIO
@@ -67,8 +79,6 @@ def test_extract_op(transformer_factory):
         t = ex.transformer
         ex()
         x_out = serde_weights.extract_op(t, x_op)
-        print("x_out:\n", x_out)
-        print("np.ones(axes.lengths):\n", np.ones(axes.lengths))
     assert (x_out == np.ones(axes.lengths)).all()
 
 
