a45ac2fa601a2935fe8cc9cdf9aa5b10cd607bf3
Henry Chang <hengluchang@gmail.com>
2017-08-08 12:14:25 -0700

    add data downloader

--- ngraph/frontends/neon/data/tsp.py
+++ ngraph/frontends/neon/data/tsp.py
@@ -15,6 +15,14 @@
 from ngraph.util.persist import valid_path_append
 from tqdm import tqdm
 import numpy as np
+import requests
+import zipfile
+import os
+
+GOOGLE_DRIVE_IDS = {
+    'tsp5_train.zip': '0B2fg8yPGn2TCSW1pNTJMXzFPYTg',
+    'tsp10_train.zip': '0B2fg8yPGn2TCbHowM0hfOTJCNkU'
+}
 
 
 class TSP(object):
@@ -34,7 +42,15 @@ class TSP(object):
         for phase in ['train', 'test']:
             filename = self.filemap[phase]['filename']
             workdir, filepath = valid_path_append(self.path, '', filename)
+            if not os.path.exists(filepath):
+                for file_name, file_id in GOOGLE_DRIVE_IDS.items():
+                    destination = './' + file_name
+                    download_file_from_google_drive(file_id, destination)
+                    with zipfile.ZipFile(destination, 'r') as z:
+                        z.extractall('./')
+                    print('\nDownloaded and unzipped {} from paper\n'.format(file_name))
 
+            print('Loading and preprocessing TSP data...')
             with open(filepath, 'r') as f:
                 X, y, y_teacher = [], [], []
                 for i, line in tqdm(enumerate(f)):
@@ -49,3 +65,39 @@ class TSP(object):
             self.data_dict[phase] = {'inp_txt': X, 'tgt_txt': y, 'teacher_tgt': y_teacher}
 
         return self.data_dict
+
+
+def download_file_from_google_drive(id, destination):
+    """
+    code based on
+    https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive/39225039#39225039
+    """
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': id}, stream=True, verify=False)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True, verify=False)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
