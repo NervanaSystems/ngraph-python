8afc64354eb4fa8877d70d619c2e9ce842783d27
rblaczkowski <rafal.blaczkowski@intel.com>
2017-08-09 09:59:26 +0200

    test_prod_constant- shorter fixture

--- tests/transformer_dependent/test_execution.py
+++ tests/transformer_dependent/test_execution.py
@@ -209,26 +209,25 @@ def test_reduction_deriv(transformer_factory, reduction, sub_axes):
 
 
 @pytest.fixture(params=[
-    (0, "A0"),
-    (1, "A1"),
-    (2, "A2"),
-    ((0, 1), "A0, A1"),
-    ((0, 2), "A0, A2"),
-    ((1, 2), "A1, A2"),
+    (0, ["A0"]),
+    (1, ["A1"]),
+    (2, ["A2"]),
+    ((0, 1), ["A0", "A1"]),
+    ((0, 2), ["A0", "A2"]),
+    ((1, 2), ["A1", "A2"]),
     pytest.config.flex_disabled(
-        ((0, 1, 2), "A0, A1, A2"),
+        ((0, 1, 2), ["A0", "A1", "A2"]),
         reason="Too big values for Flex ( > 32767 )"
     )
 ])
 def prod_constant(request):
-    ng_axis = []
-    axis_dict = {
-        "A0": ng.make_axis(length=2),
-        "A1": ng.make_axis(length=3),
-        "A2": ng.make_axis(length=4)}
-    for axis in request.param[1].split(', '):
-        ng_axis.append(axis_dict[axis])
-    return request.param[0], ng_axis, axis_dict
+    axes_dict = collections.OrderedDict([
+        ("A0", ng.make_axis(length=2)),
+        ("A1", ng.make_axis(length=3)),
+        ("A2", ng.make_axis(length=4))
+    ])
+    np_axis, axes_names = request.param
+    return np_axis, map(lambda x: axes_dict[x], axes_names), axes_dict.values()
 
 
 @pytest.config.argon_disabled  # TODO Triage
@@ -236,12 +235,10 @@ def test_prod_constant(transformer_factory, prod_constant):
     """
     Test reduce product of constants
     """
-    np_axis, ng_axis, axis_dict = prod_constant
-    A0, A1, A2 = axis_dict["A0"], axis_dict["A1"], axis_dict["A2"]
+    np_axis, ng_axis, axes_values = prod_constant
 
     # ngrpah op
-    const_3d = ng.broadcast(ng.constant(2., axes=[]), axes=[A0, A1, A2])
-    print ng_axis
+    const_3d = ng.broadcast(ng.constant(2., axes=[]), axes=axes_values)
     prod = ng.prod(const_3d, reduction_axes=ng_axis)
 
     # numpy results
