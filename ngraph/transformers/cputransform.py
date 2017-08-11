fix memory pool size calculation

ngraph/transformers/cputransform.py
@@ -930,18 +930,16 @@ self.__profiler_stop__  = list()
 
     def finish_load_computation(self, computation_decl):
         device_computation = computation_decl.device_computation
-        temp_pool_size = computation_decl.exop_block.memory_footprint() // 4
-        persistent_pool_size = computation_decl.exop_block.persistent_size() // 4
         byte_alignment = computation_decl.execution_graph.execution_state \
             .transformer.byte_alignment
         self.exop_codegen_pools.append(
             "{}_temporary_pool = align_ndarray({}, {}, np.dtype('{}'))",
-            computation_decl.computation_op.name, temp_pool_size,
+            computation_decl.computation_op.name, computation_decl.temporary_max_allocated,
             byte_alignment,
             'float32')
         self.exop_codegen_pools.append(
             "{}_persistent_pool = align_ndarray({}, {}, np.dtype('{}'))",
-            computation_decl.computation_op.name, persistent_pool_size,
+            computation_decl.computation_op.name, computation_decl.persistent_max_allocated,
             byte_alignment,
             'float32')
 