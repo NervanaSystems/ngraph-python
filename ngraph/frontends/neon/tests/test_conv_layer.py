72fe0ba1c13163de08f40241142c928b4c6ad66d
Ruby Pai <ruby.j.pai@intel.com>
2017-09-29 11:39:08 -0700

    consolidate conv tests

--- ngraph/frontends/neon/tests/test_conv_layer.py
+++ ngraph/frontends/neon/tests/test_conv_layer.py
@@ -1,9 +1,37 @@
 import pytest
 import numpy as np
+from contextlib import closing
 import ngraph as ng
+import ngraph.transformers as ngt
 from ngraph.testing import executor
+from ngraph.frontends.common import utils
 from ngraph.op_graph.axes import IncompatibleAxesError
-from ngraph.frontends.neon import Convolution
+from ngraph.frontends.neon import Convolution, Deconvolution
+from ngraph.frontends.neon import ConstantInit, Rectlin, GaussianInit
+
+
+def reference_conv1d(inputs, filters, activation, strides=1, padding=0):
+    # for now:
+    assert strides == 1
+    assert padding == 0
+    # inputs: features, time steps (conv axis), batch size
+    # filters: input feature dimension/channels, T=1, R=filter_width, S=1, K=num_filters
+    # result: K, 1, time_steps - S + 1, 1, batch size
+    filters = np.squeeze(filters)  # input channels, filter_width, num_filters
+    feature_dimension, time_steps_in, batch_size = inputs.shape
+    filter_width = filters.shape[1]
+    K = filters.shape[-1]
+    time_steps_out = time_steps_in - filter_width + 1
+    result = np.zeros((K, time_steps_out, batch_size))
+    # TODO: refactor to make this more efficient
+    for t in range(time_steps_out):
+        for k in range(K):
+            for n in range(batch_size):
+                result[k, t, n] = np.sum(inputs[:, t:t+filter_width, n] * filters[:, :, k])
+    result = activation(result)
+    # expand dimensions from K, time_steps, batch_size to (K, 1, time_steps, 1, batch_size)
+    result = np.expand_dims(np.expand_dims(result, axis=1), axis=3)
+    return result
 
 
 # TODO: Remove these to conftest.py
@@ -116,3 +144,111 @@ def test_alternate_channel_axes(conv1d_placeholder, output_size, channel_axis):
         conv_layer(conv1d_placeholder)
     output = conv_layer(conv1d_placeholder, channel_axes="channel")
     assert output.axes == conv1d_placeholder.axes
+
+
+@pytest.mark.parametrize('filter_width', [3])
+@pytest.mark.parametrize('num_filters', [2])
+@pytest.mark.parametrize('strides', [1])
+@pytest.mark.parametrize('padding', [0])
+@pytest.mark.parametrize('time_steps', [5])
+@pytest.mark.parametrize('feature_dimension', [4])
+@pytest.mark.parametrize('batch_size', [2])
+def test_conv1d(transformer_factory, filter_width, num_filters, strides, padding,
+                time_steps, feature_dimension, batch_size):
+
+    dilation = 1  # reference conv does not support dilation
+
+    F = ng.make_axis(name='F', length=feature_dimension)
+    REC = ng.make_axis(name='REC', length=time_steps)
+    N = ng.make_axis(name='N', length=batch_size)
+    in_axes = ng.make_axes([F, REC, N])
+
+    inputs = ng.placeholder(axes=in_axes)
+    input_vals = np.random.randn(*in_axes.lengths)
+
+    filter_init = GaussianInit()
+
+    conv1d = Convolution((filter_width, num_filters), filter_init,
+                           strides=strides, padding=padding, dilation=dilation,
+                           bias_init=None, activation=Rectlin(), batch_norm=None)
+
+    result_op = conv1d(inputs, channel_axes='F', spatial_axes={'W': 'REC'})
+
+    with closing(ngt.make_transformer()) as transformer:
+        result_comp = transformer.add_computation(ng.computation(result_op, inputs))
+        filter_vals = transformer.add_computation(ng.computation(conv1d.conv.W))()
+
+        result_ng = result_comp(input_vals)
+        result_np = np.squeeze(reference_conv1d(input_vals, filter_vals, lambda x: np.maximum(0, x)))
+        ng.testing.assert_allclose(result_ng, result_np)
+
+
+# TODO: add other configurations?
+@pytest.mark.transformer_dependent
+@pytest.config.flex_disabled(reason="#1841, deconv is not yet supported by flex")
+def test_deconv():
+    """
+    basic test of deconv fprop.
+    ngraph/tests/test_conv.py tests ng.deconvolution bprop
+    """
+
+    # filter params
+    R, S = 5, 5
+    fshape = (R, S, 1)
+    strides = 2
+    filter_val_nz = np.arange(1, R * S + 1).reshape(R, S)
+    filter_val = np.zeros(fshape)
+    filter_val[:, :, 0] = filter_val_nz
+
+    deconv = Deconvolution(fshape,
+                           filter_init=ConstantInit(filter_val),
+                           strides=strides,
+                           padding=0,
+                           dilation=1)
+
+    N = ng.make_axis(name='N', length=1)  # batch
+    image_shape = (1, 8, 8)  # CHW
+    image_axes = ng.make_axes([ng.make_axis(name=nm, length=l)
+                               for nm, l in zip('CHW', image_shape)])
+    image_axes |= N
+    image = ng.placeholder(axes=image_axes)
+
+    output = deconv(image)
+
+    with closing(ngt.make_transformer()) as transformer:
+        comp = transformer.add_computation(ng.computation(output, image))
+        input_val = np.zeros(image_shape + (N.length, ), dtype=float)
+        input_val[0, 0, 0] = 1
+        input_val[0, 5, 5] = 1
+        input_val[0, 7, 7] = 1
+        result = comp(input_val)
+        feature_map = np.squeeze(result)
+
+        assert (feature_map[:5, :5] == filter_val_nz).all()
+
+        result2 = filter_val_nz.copy()
+        result2[-1, -1] = 26
+        assert (feature_map[10:15, 10:15] == result2).all()
+
+        result3 = filter_val_nz.copy()
+        result3[0, 0] = 26
+        assert (feature_map[-5:, -5:] == result3).all()
+
+
+@pytest.mark.parametrize("input_size", (10, 25))
+@pytest.mark.parametrize("filter_size", (3, 4))
+@pytest.mark.parametrize("padding", ((0, 0), (3, 4)))
+@pytest.mark.parametrize("stride", (1, 3))
+def test_conv_inverts_deconv(transformer_factory, input_size, filter_size, padding, stride):
+    """ Test that conv and deconv are inverse operations given the same parameters"""
+
+    # convolutions whose output size are not an even multiple of stride cannot be exactly inverted
+    a = (input_size + sum(padding) - filter_size) % stride
+    conv_output = utils.conv_output_dim(input_size, filter_size, padding, stride)
+    deconv_output = utils.deconv_output_dim(conv_output, filter_size, padding, stride)
+
+    assert deconv_output == (input_size - a), ("Convolution and Deconvolution do not invert:\n"
+                                               "output ({}) != input ({}) - a ({})\n"
+                                               "filter: {}, padding: {}, stride: {}"
+                                               ).format(deconv_output, input_size, a,
+                                                        filter_size, padding, stride)
