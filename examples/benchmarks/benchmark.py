b9d900667fa53423eef7ca0c42b1332c3916fb4c
wconstab <willconstable@gmail.com>
2017-10-03 16:50:23 -0700

    fix style

--- examples/benchmarks/benchmark.py
+++ examples/benchmarks/benchmark.py
@@ -87,7 +87,7 @@ class Benchmark(object):
         start = Benchmark.marker.init_mark()
         end = Benchmark.marker.init_mark()
         t_args = {}
-        if self.transformer=='hetr':
+        if self.transformer == 'hetr':
             t_args['device'] = self.device
         with closing(ngt.make_transformer_factory(self.transformer, **t_args)()) as transformer:
             if visualize:
