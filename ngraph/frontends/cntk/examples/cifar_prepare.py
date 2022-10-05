fde9242978b190af148530f952991d16a8dd633f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 01:59:34 +0000

    Adding numeric_owner as keyword arguement

--- ngraph/frontends/cntk/examples/cifar_prepare.py
+++ ngraph/frontends/cntk/examples/cifar_prepare.py
@@ -70,7 +70,7 @@ def loadData(src):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar)
